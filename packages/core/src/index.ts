edd5a62f3c603d8042684e2536da1634107f02d9
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-02 19:33:23 +0100

    feat(#319): AuthenticationService
    - refactor .well-known/jwks.json to use JWKSService

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,10 @@
 import 'server-only';
 
+export * from './components';
 export * from './config/types';
 export * from './container';
 export * from './container/tokens';
 export * from './context/types';
-export * from './components';
+export * from './infrastructure';
 
 export { defaultConfig } from './config/defaults';
