19baadcf9d01197c6a6fea133eefebe06f3c500f
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-10 12:16:16 +0100

    feat(#287): add governance repository integration tests

--- packages/core/src/components/governance/repository.ts
+++ packages/core/src/components/governance/repository.ts
@@ -1,8 +1,9 @@
 import { Repository } from '../../container/types';
-import { Document } from './types';
+import { Document, CreateDocument } from './types';
 
 export interface DocumentRepository extends Repository {
   findById(id: number): Promise<Document | null>;
   findBySlug(slug: string): Promise<Document | null>;
   findAll(): Promise<Document[]>;
+  create(values: CreateDocument): Promise<Document>;
 }
