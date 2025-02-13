15d795cca77ba69aff4840ba7f79a499bdeecf4d
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-13 16:58:01 +0300

    fix(EthAdress): formatting

--- packages/epics/src/profile/components/eth-address.tsx
+++ packages/epics/src/profile/components/eth-address.tsx
@@ -9,17 +9,14 @@ interface EthAdressProps {
 export const EthAddress = ({ address, hasCopyButton }: EthAdressProps) => {
   const copy = (text: string) => {
     if (hasCopyButton) {
-      copyToClipboard(text)
+      copyToClipboard(text);
     }
   };
 
   if (!address) return null;
 
   return (
-    <div
-      onClick={() => copy(address)}
-      className="w-full flex justify-between"
-    >
+    <div onClick={() => copy(address)} className="w-full flex justify-between">
       {`${address.slice(0, 6)}…${address.slice(-4)}`}
       {hasCopyButton ? <CopyIcon className="icon-sm ml-2" /> : null}
     </div>
