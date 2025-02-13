ec0154e0a77a2b97da4e373049e0f525d007a807
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-13 14:26:05 +0300

    feat(ProfileButton): added new items

--- packages/epics/src/profile/components/button-profile.tsx
+++ packages/epics/src/profile/components/button-profile.tsx
@@ -5,12 +5,12 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@hypha-platform/ui';
 import { PersonAvatar } from './person-avatar';
 import { EthAddress } from './eth-address';
+import { TrashIcon, LogOutIcon } from 'lucide-react';
 
 export type ButtonProfileProps = {
   avatarSrc?: string;
@@ -19,6 +19,9 @@ export type ButtonProfileProps = {
   isConnected: boolean;
   login: () => void;
   logout: () => void;
+  deleteProfile?: () => void;
+  transitionToProfile?: () => void;
+  transitionToEdit?: () => void;
 };
 
 export const ButtonProfile = ({
@@ -28,6 +31,9 @@ export const ButtonProfile = ({
   address,
   login,
   logout,
+  deleteProfile,
+  transitionToProfile,
+  transitionToEdit,
 }: ButtonProfileProps) => {
   return (
     <div>
@@ -37,12 +43,25 @@ export const ButtonProfile = ({
             <PersonAvatar avatarSrc={avatarSrc} userName={userName} />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <EthAddress address={address} />
+            <DropdownMenuItem onClick={transitionToProfile} className='text-1'>
+              User profile
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
+            <DropdownMenuItem onClick={transitionToEdit} className='text-1'>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem className='text-1 flex justify-between'>
+              <EthAddress address={address} hasCopyButton />
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={deleteProfile} className='text-1 flex justify-between'>
+              Delete
+              <TrashIcon className='icon-sm' />
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={logout} className='text-1 text-error-11 flex justify-between'>
+              Logout
+              <LogOutIcon className='icon-sm' />
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       ) : (
