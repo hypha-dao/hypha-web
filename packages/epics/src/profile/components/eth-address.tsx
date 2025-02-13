74092980a9b6d5608251c2a5dca7e1b9cffd3988
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-13 14:24:26 +0300

    feat(EthAddress): added copyToClipboard action

--- packages/epics/src/profile/components/eth-address.tsx
+++ packages/epics/src/profile/components/eth-address.tsx
@@ -1,4 +1,23 @@
-export const EthAddress = ({ address }: { address?: string }) => {
+import { CopyIcon } from '@radix-ui/react-icons';
+
+interface EthAdressProps {
+  address?: string,
+  hasCopyButton?: boolean,
+}
+
+export const EthAddress = ({ address, hasCopyButton }: EthAdressProps) => {
+  const copyToClipboard = (text: string) => {
+    if (hasCopyButton) {
+      navigator.clipboard.writeText(text);
+    }
+  };
+
   if (!address) return null;
-  return `${address.slice(0, 6)}…${address.slice(-4)}`;
+
+  return (
+    <div onClick={() => copyToClipboard(address)} className="w-full flex justify-between">
+      {`${address.slice(0, 6)}…${address.slice(-4)}`}
+      {hasCopyButton ? <CopyIcon className='icon-sm ml-2' /> : null}
+    </div>
+  );
 };
