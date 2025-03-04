15b2f0a9ec15c1ea1458c8d102b4607a69363035
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-04 14:17:33 +0300

    feat(#485): type fix

--- apps/web/src/app/[lang]/profile/page.tsx
+++ apps/web/src/app/[lang]/profile/page.tsx
@@ -58,7 +58,7 @@ export default function Profile() {
         </Link>
         <Text className="text-sm text-neutral-11 ml-1">/ Profile Page</Text>
       </div>
-      <PersonHead {...personHeadProps} lang={lang} />
+      <PersonHead {...personHeadProps} lang={lang as Locale} />
       <div className="mt-6">
         <MemberSpaces spaces={[]} profileView />
       </div>
