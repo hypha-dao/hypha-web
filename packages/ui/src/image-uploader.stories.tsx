6ecc0b8efc04f9549a999169a58b2f9c78fc7cf4
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-20 16:04:35 +0300

    feat(#614): make ImageUploader like compound component

--- packages/ui/src/image-uploader.stories.tsx
+++ packages/ui/src/image-uploader.stories.tsx
@@ -1,25 +1,40 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import { ImageUploader } from './image-uploader';
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta = {
   component: ImageUploader,
-  title: 'UI/Profile/ImageUploader',
+  title: 'UI/ImageUploader',
+  args: {
+    isUploading: false,
+    uploadedFile: null,
+    onReset: () => {},
+    onUpload: () => {},
+  },
 } satisfies Meta<typeof ImageUploader>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-type Story = StoryObj<typeof ImageUploader>;
-
-export const Default: Story = {
+export const WithAvatar: Story = {
   args: {
     isUploading: false,
-    uploadedFile: '',
+    uploadedFile: null,
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/Upload/gi)).toBeTruthy();
+  render: (args) => (
+    <ImageUploader {...args}>
+      <ImageUploader.Avatar size="lg" />
+    </ImageUploader>
+  ),
+};
+
+export const WithLeadImage: Story = {
+  args: {
+    isUploading: false,
+    uploadedFile: null,
   },
+  render: (args) => (
+    <ImageUploader {...args}>
+      <ImageUploader.Lead />
+    </ImageUploader>
+  ),
 };
