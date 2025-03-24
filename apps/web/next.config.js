d7af957e87cd687bb8f29aeb6c6b6b28b084f910
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-24 10:35:43 +0300

    feat(#485): removed unused props and fixed avatar displaying

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -74,6 +74,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'utfs.io'
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.githubassets.com'
       }
     ],
   },
