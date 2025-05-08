3e8d84ea860113934e8317a3012800d7d5363b5a
n0umen0n <hramtsovvladislav@gmail.com>
2025-05-08 11:49:00 +0300

    claimrewards by anyone, new destinations for USDC and HYPHA, tests

--- packages/storage-evm/contracts/HyphaToken.sol
+++ packages/storage-evm/contracts/HyphaToken.sol
@@ -66,6 +66,24 @@ contract HyphaToken is
   }
 
   /**
+   * @dev Set destination addresses for transfers
+   * @param _iexAddress Address to receive USDC from payForSpaces and HYPHA from payInHypha
+   * @param _mainHyphaAddress Address to receive USDC from investInHypha
+   */
+  function setDestinationAddresses(
+    address _iexAddress,
+    address _mainHyphaAddress
+  ) external onlyOwner {
+    require(_iexAddress != address(0), 'Invalid IEX address');
+    require(_mainHyphaAddress != address(0), 'Invalid mainHypha address');
+
+    iexAddress = _iexAddress;
+    mainHyphaAddress = _mainHyphaAddress;
+
+    emit DestinationAddressesUpdated(_iexAddress, _mainHyphaAddress);
+  }
+
+  /**
    * @dev Pay for multiple spaces with USDC and distribute HYPHA tokens to the rewards pool
    * @param spaceIds Array of space IDs to pay for
    * @param usdcAmounts Array of USDC amounts for each space
@@ -79,6 +97,7 @@ contract HyphaToken is
       'Input arrays length mismatch'
     );
     require(spaceIds.length > 0, 'No spaces specified');
+    require(iexAddress != address(0), 'IEX address not set');
 
     // Calculate total USDC amount and durations
     uint256 totalUsdcAmount = 0;
@@ -97,10 +116,9 @@ contract HyphaToken is
     // Update distribution state before adding new tokens to the distribution
     updateDistributionState();
 
-    // Transfer total USDC from user
-    // User will have to approve the USDC spenditure, and then the transferFrom will be successful, so two separate transactions
+    // Transfer total USDC from user to the IEX address instead of this contract
     require(
-      usdc.transferFrom(msg.sender, address(this), totalUsdcAmount),
+      usdc.transferFrom(msg.sender, iexAddress, totalUsdcAmount),
       'USDC transfer failed'
     );
 
@@ -143,12 +161,14 @@ contract HyphaToken is
    * @param usdcAmount Amount of USDC to invest
    */
   function investInHypha(uint256 usdcAmount) external {
+    require(mainHyphaAddress != address(0), 'MainHypha address not set');
+
     // Update distribution state before minting new tokens
     updateDistributionState();
 
-    // Transfer USDC from investor
+    // Transfer USDC from investor to the mainHypha address
     require(
-      usdc.transferFrom(msg.sender, address(this), usdcAmount),
+      usdc.transferFrom(msg.sender, mainHyphaAddress, usdcAmount),
       'USDC transfer failed'
     );
 
@@ -245,23 +265,24 @@ contract HyphaToken is
 
   /**
    * @dev Claim accumulated rewards
+   * @param account Address of the user to claim rewards for
    */
-  function claimRewards() external {
+  function claimRewards(address account) external {
     updateDistributionState();
 
-    uint256 reward = pendingRewards(msg.sender);
+    uint256 reward = pendingRewards(account);
     if (reward > 0) {
       // Ensure we don't exceed max supply
       require(totalMinted + reward <= MAX_SUPPLY, 'Exceeds max token supply');
 
-      unclaimedRewards[msg.sender] = 0;
-      userRewardDebt[msg.sender] = accumulatedRewardPerToken;
+      unclaimedRewards[account] = 0;
+      userRewardDebt[account] = accumulatedRewardPerToken;
 
       // Mint reward tokens to the user
-      _mint(msg.sender, reward);
+      _mint(account, reward);
       totalMinted = totalMinted + reward;
 
-      emit RewardsClaimed(msg.sender, reward);
+      emit RewardsClaimed(account, reward);
     }
   }
 
@@ -295,6 +316,7 @@ contract HyphaToken is
       'Input arrays length mismatch'
     );
     require(spaceIds.length > 0, 'No spaces specified');
+    require(iexAddress != address(0), 'IEX address not set');
 
     // Calculate total HYPHA required and durations
     uint256 totalHyphaRequired = 0;
@@ -316,11 +338,11 @@ contract HyphaToken is
       'Insufficient HYPHA balance'
     );
 
-    // Update distribution state before burning tokens
+    // Update distribution state before transferring tokens
     updateDistributionState();
 
-    // Burn HYPHA tokens from the user's account as payment
-    _burn(msg.sender, totalHyphaRequired);
+    // Transfer HYPHA tokens from the user to the IEX address instead of burning
+    _transfer(msg.sender, iexAddress, totalHyphaRequired);
 
     // Calculate additional HYPHA to be distributed as rewards
     uint256 distributionAmount = totalHyphaRequired * distributionMultiplier;
