85f8dfb0904713f89a780c65b6bf0eb6517a4a91
Dmitry E <dsanichweb@gmail.com>
2025-06-11 11:07:48 +0300

    feat(#877): improved reset members on switch voting method

--- packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
+++ packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
@@ -22,12 +22,17 @@ export const ChangeVotingMethodPlugin = ({
   const handleMethodChange = (method: string | null) => {
     setSelectedMethod(method);
     setValue('votingMethod', method as '1m1v' | '1v1v' | '1t1v');
-    setValue('members', [
-      {
-        member: undefined,
-        number: undefined,
-      },
-    ]);
+    setValue(
+      'members',
+      method === '1v1v'
+        ? [
+            {
+              member: undefined,
+              number: undefined,
+            },
+          ]
+        : [],
+    );
     setValue('token', undefined);
   };
 
