6e343784c52a0580b2374475a02c227f93030bbd
Sergey B. <sergey3bv@gmail.com>
2025-06-11 11:19:29 +0300

    Functions to get balance and token symbol were added

---
+++ packages/core/src/common/web3/get-token-symbol.ts
@@ -0,0 +1,21 @@
+'use client';
+
+import { publicClient } from '@core/common/web3/public-client';
+import { erc20Abi, getContract } from 'viem';
+
+export async function getTokenSymbol(
+  tokenAddress: `0x${string}`,
+): Promise<string> {
+  const contract = getContract({
+    address: tokenAddress,
+    abi: erc20Abi,
+    client: publicClient,
+  });
+
+  try {
+    return await contract.read.symbol();
+  } catch (error: any) {
+    console.error(`Failed to fetch symbol for token ${tokenAddress}:`, error);
+    throw new Error(`Could not retrieve token symbol: ${error.message}`);
+  }
+}
--- packages/core/src/common/web3/index.ts
+++ packages/core/src/common/web3/index.ts
@@ -1,2 +1,4 @@
 export * from './public-client';
 export * from './get-token-decimals';
+export * from './get-token-symbol';
+export * from './get-balance';
