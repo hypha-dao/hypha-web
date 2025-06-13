2389e71defde02ffbb55ba5d49d9f0425ff9715e
Philipp Litzenberger <philipp@bendcircular.com>
2025-06-13 08:56:24 +0200

    feat(#914): Configure Production Node

--- packages/core/src/common/web3/public-client.ts
+++ packages/core/src/common/web3/public-client.ts
@@ -2,6 +2,9 @@ import { createPublicClient, http } from 'viem';
 import { base } from 'viem/chains';
 
 export const publicClient = createPublicClient({
+  batch: {
+    multicall: true,
+  },
   chain: base,
-  transport: http(),
+  transport: http(process.env.NEXT_PUBLIC_RPC_URL),
 });
