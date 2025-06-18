7fb492924230ff70c3d4565823f792caba5bdfa2
Sergey B. <sergey3bv@gmail.com>
2025-06-18 14:00:20 +0300

    Moralis client replaced with getter

--- packages/core/src/common/web3/moralis-client.ts
+++ packages/core/src/common/web3/moralis-client.ts
@@ -1,16 +1,18 @@
 import { base } from 'viem/chains';
 import Moralis from 'moralis';
 
-async function getMoralis() {
-  if (!Moralis.Core.isStarted) {
-    await Moralis.start({
-      apiKey: process.env.MORALIS_API_KEY,
-      defaultNetwork: base.id,
-      defaultEvmApiChain: base.id,
-    });
-  }
+let moralisInitPromise: Promise<typeof Moralis> | null = null;
 
+export async function getMoralis(): Promise<typeof Moralis> {
+  if (!Moralis.Core.isStarted) {
+    if (!moralisInitPromise) {
+      moralisInitPromise = Moralis.start({
+        apiKey: process.env.MORALIS_API_KEY,
+        defaultNetwork: base.id,
+        defaultEvmApiChain: base.id,
+      }).then(() => Moralis);
+    }
+    await moralisInitPromise;
+  }
   return Moralis;
 }
-
-export const moralisClient: typeof Moralis = await getMoralis();
