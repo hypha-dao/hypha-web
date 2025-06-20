dd9003632c81135d0fa2983a31bb7460e0175412
n0umen0n <hramtsovvladislav@gmail.com>
2025-06-20 13:48:46 +0300

    removed function declaration

--- packages/storage-evm/contracts/OwnershipSpaceToken.sol
+++ packages/storage-evm/contracts/OwnershipSpaceToken.sol
@@ -138,4 +138,8 @@ contract OwnershipSpaceToken is SpaceToken {
     require(_isSpaceMember(to), 'Can only mint to space members');
     super.mint(to, amount);
   }
+
+  function escrowContract() external view returns (address) {
+    return escrowContract;
+  }
 }
