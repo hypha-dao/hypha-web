590142cac245d64cee64ea3b4e64599afb3f9f70
Philipp Litzenberger <philipp@bendcircular.com>
2025-06-23 17:23:26 +0200

    feat(#914): Configure Production Node

--- packages/evm/src/config.ts
+++ packages/evm/src/config.ts
@@ -3,9 +3,12 @@ import { http } from '@wagmi/core';
 import { hardhat, base } from '@wagmi/core/chains';
 
 export const config = createConfig({
+  batch: {
+    multicall: true,
+  },
   chains: [hardhat, base],
   transports: {
-    [base.id]: http(),
+    [base.id]: http(process.env.NEXT_PUBLIC_RPC_URL),
     [hardhat.id]: http(),
   },
 });
