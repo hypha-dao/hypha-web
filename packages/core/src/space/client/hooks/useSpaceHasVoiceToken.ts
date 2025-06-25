f195aa080b25f7cc74f0539ba2e28ef4c37ffe87
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-06-25 14:13:19 +0300

    feat(#878): created useSpaceHasVoiceToken hook

---
+++ packages/core/src/space/client/hooks/useSpaceHasVoiceToken.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import { publicClient } from '@core/common';
+import { getSpaceDecayingTokens } from '../web3';
+import useSWR from 'swr';
+
+export const useSpaceHasVoiceToken = ({ spaceId }: { spaceId: bigint }) => {
+  const { data, isLoading, error } = useSWR(
+    [spaceId, 'decayingTokens'],
+    async ([spaceId]) =>
+      publicClient.readContract(getSpaceDecayingTokens({ spaceId })),
+    { revalidateOnFocus: true },
+  );
+  console.log('data', data);
+  const hasVoiceToken = data?.length !== 0;
+
+  return {
+    hasVoiceToken,
+    isLoading,
+    error,
+  };
+};
