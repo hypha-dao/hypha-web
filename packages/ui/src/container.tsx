c7e0f1c20a0259379e58932d5777b9e0fdc70c6f
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-06-25 16:13:01 -0300

    fix: container sizes and screen width breakpoint

--- packages/ui/src/container.tsx
+++ packages/ui/src/container.tsx
@@ -6,5 +6,12 @@ type Props = {
 };
 
 export const Container = ({ children, className }: Props) => {
-  return <div className={clsx('container mx-auto', className)}>{children}</div>;
+  // Tailwind doesn't have a native way to say:
+  // "Use screen breakpoint 2xl (1536px), but apply a container max-width of 1100px."
+  // So we need to use custom classes for this.
+  return <div className={clsx(
+    'container mx-auto',
+    'max-w-[64rem] md:max-w-[76rem] lg:max-w-[81.2rem] 2xl:max-w-[110rem]',
+    className
+  )}>{children}</div>;
 };
