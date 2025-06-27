c323a5e4b551f4271cd3c21487c6879a6a267c50
Sergey B. <sergey3bv@gmail.com>
2025-06-27 12:57:44 +0300

    refactor: price fetching was moved to a function

---
+++ packages/core/src/common/web3/get-token-price.ts
@@ -0,0 +1,25 @@
+import { getMoralis } from './moralis-client';
+
+export async function getTokenPrice(
+  tokens: `0x${string}`[],
+  chainId = '0x2105',
+) {
+  const moralis = await getMoralis();
+
+  const { result } = await moralis.EvmApi.token.getMultipleTokenPrices(
+    { chain: chainId },
+    {
+      tokens: tokens.map((token) => ({ tokenAddress: token })),
+    },
+  );
+
+  return Object.fromEntries(
+    tokens.map((token) => [
+      token,
+      result.find(
+        (r) =>
+          r.tokenAddress === token || r.tokenAddress === token.toLowerCase(),
+      )?.usdPrice,
+    ]),
+  );
+}
