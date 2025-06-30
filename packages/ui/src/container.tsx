4309c2bbea53c4ec2859a57c64e03ef7d868b796
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-06-29 21:08:34 -0300

    chore: update container sizes

--- packages/ui/src/container.tsx
+++ packages/ui/src/container.tsx
@@ -3,15 +3,24 @@ import clsx from 'clsx';
 type Props = {
   children?: React.ReactNode;
   className?: string;
+  size?: 'sm' | 'md' | 'lg';
 };
 
-export const Container = ({ children, className }: Props) => {
+export const Container = ({ children, size = 'md', className }: Props) => {
   // Tailwind doesn't have a native way to say:
   // "Use screen breakpoint 2xl (1536px), but apply a container max-width of 1100px."
   // So we need to use custom classes for this.
-  return <div className={clsx(
-    'container mx-auto',
-    'max-w-[64rem] md:max-w-[76rem] lg:max-w-[81.2rem] 2xl:max-w-[110rem]',
-    className
-  )}>{children}</div>;
+  return (
+    <div
+      className={clsx(
+        'container mx-auto',
+        'max-w-[--spacing-container-sm] md:max-w-[--spacing-container-md]',
+        size == "md" && 'lg:max-w-[--spacing-container-lg] xl:max-w-[--spacing-container-xl]',
+        size == "lg" && 'lg:max-w-[--spacing-container-xl] xl:max-w-[--spacing-container-2xl]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 };
--- packages/ui/src/organisms/menu-top.tsx
+++ packages/ui/src/organisms/menu-top.tsx
@@ -25,18 +25,24 @@ export const MenuTop = ({
   withLogo,
 }: MenuTopProps) => {
   return (
-    <div className="fixed top-0 right-0 left-0 flex items-center space-x-10 h-9 px-10 bg-background z-10">
-      {withLogo ? <Logo width={140}></Logo> : null}
-      <div id="menu-top-active-dao" className="flex-grow">
-        {activeDao && <h1 className="text-2xl">{activeDao.title}</h1>}
+    <header className="fixed top-0 right-0 left-0 flex items-center h-9 px-10 bg-background z-10">
+      <div className="w-full max-w-[--spacing-container-2xl] mx-auto flex items-center justify-between space-x-10">
+        {withLogo ? <Logo width={140}></Logo> : null}
+        <div id="menu-top-active-dao" className="flex-grow">
+          {activeDao && <h1 className="text-2xl">{activeDao.title}</h1>}
+        </div>
+        <div id="menu-top-actions" className="flex justify-center gap-2">
+          {navItems.map((item) => (
+            <ButtonNavItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+            />
+          ))}
+        </div>
+        {children}
       </div>
-      <div id="menu-top-actions" className="flex justify-center gap-2">
-        {navItems.map((item) => (
-          <ButtonNavItem key={item.href} href={item.href} label={item.label} />
-        ))}
-      </div>
-      {children}
-    </div>
+    </header>
   );
 };
 
