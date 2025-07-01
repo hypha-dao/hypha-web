401a6de9d72c875ea37c8b38127170531fe2d137
Dmitry E <dsanichweb@gmail.com>
2025-07-01 11:00:53 +0300

    chore(#982): fixed missing hostname for remote images

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -86,6 +86,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
