0f1ac85693183d2128d336688f22ea02ce4e62d2
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-03 00:30:24 -0300

    chore: update logo and header for light theme

--- packages/ui/src/atoms/logo/index.tsx
+++ packages/ui/src/atoms/logo/index.tsx
@@ -1,16 +1,26 @@
-import Image from 'next/image';
-import LogoSrc from './logo-white.svg';
+'use client';
 
-type logoProps = {
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import LogoDark from './logo-white.svg'; // for dark theme
+import LogoLight from './logo-black.svg'; // for light theme
+
+type LogoProps = {
   width?: number;
   height?: number;
 };
 
-export const Logo = ({ width, height }: logoProps) => (
-  <Image
-    src={LogoSrc}
-    alt="Logo"
-    width={width ? width : 100}
-    height={height ? height : 100}
-  />
-);
+export const Logo = ({ width = 100, height = 100 }: LogoProps) => {
+  const { resolvedTheme } = useTheme();
+
+  const logoSrc = resolvedTheme === 'dark' ? LogoDark : LogoLight;
+
+  return (
+    <Image
+      src={logoSrc}
+      alt="Logo"
+      width={width}
+      height={height}
+    />
+  );
+};
--- packages/ui/src/organisms/menu-top.tsx
+++ packages/ui/src/organisms/menu-top.tsx
@@ -25,7 +25,7 @@ export const MenuTop = ({
   withLogo,
 }: MenuTopProps) => {
   return (
-    <div className="fixed top-0 right-0 left-0 flex items-center space-x-10 h-9 px-10 bg-background z-10">
+    <div className="fixed top-0 right-0 left-0 flex items-center space-x-10 h-9 px-10 z-10 bg-inherit">
       {withLogo ? <Logo width={140}></Logo> : null}
       <div id="menu-top-active-dao" className="flex-grow">
         {activeDao && <h1 className="text-2xl">{activeDao.title}</h1>}
