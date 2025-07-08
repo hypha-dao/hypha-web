180646e62d06b2f33e7a95d6ac210b6e1045023f
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-08 17:05:09 +0300

    feat(#814): implemented useAddMemberOrchestrator

--- packages/epics/src/spaces/components/join-space.tsx
+++ packages/epics/src/spaces/components/join-space.tsx
@@ -6,34 +6,58 @@ import { PersonIcon } from '@radix-ui/react-icons';
 import React from 'react';
 import { Loader2 } from 'lucide-react';
 import { useSpaceDetailsWeb3Rpc } from '@core/space';
+import { useMe, useJwt } from '@core/people';
+import { useAddMemberOrchestrator } from '@core/governance';
+import { useConfig } from 'wagmi';
 
 type JoinSpaceProps = {
   spaceId: number;
 };
 
 export const JoinSpace = ({ spaceId }: JoinSpaceProps) => {
+  const config = useConfig();
+  const { jwt } = useJwt();
   const { spaceDetails } = useSpaceDetailsWeb3Rpc({ spaceId: spaceId });
   const isInviteOnly = spaceDetails?.joinMethod === 2n;
+
+  const { person } = useMe();
 
   const { isMember, isLoading, joinSpace, revalidateIsMember, isJoiningSpace } =
     useJoinSpace({
       spaceId,
     });
 
+  const { requestInvite, isCreating } = useAddMemberOrchestrator({
+    authToken: jwt,
+    config: config,
+    spaceId: spaceId,
+    memberAddress: person?.address as `0x${string}`,
+  });
+
   const handleJoinSpace = React.useCallback(async () => {
-    await joinSpace();
-    revalidateIsMember();
+    if (isInviteOnly) {
+      console.log('123123');
+      await requestInvite({
+        spaceId: spaceId,
+        title: 'Invite Member',
+        description: `To onboard this member, we need as a space to approve this proposal. Member ${person?.name} ${person?.surname} [${person?.address}]`,
+        creatorId: person?.id as number,
+      });
+    } else {
+      await joinSpace();
+      revalidateIsMember();
+    }
   }, [joinSpace, revalidateIsMember]);
 
   return (
     <Button
-      disabled={isMember || isLoading || isJoiningSpace}
+      disabled={isMember || isLoading || isJoiningSpace || isCreating}
       onClick={handleJoinSpace}
       className="ml-2 rounded-lg"
       colorVariant={isMember ? 'neutral' : 'accent'}
       variant={isMember ? 'outline' : 'default'}
     >
-      {isJoiningSpace ? (
+      {isJoiningSpace || isCreating ? (
         <Loader2 className="mr-2 animate-spin" width={16} height={16} />
       ) : (
         <PersonIcon className="mr-2" width={16} height={16} />
