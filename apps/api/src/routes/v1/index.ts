7dbe6a8385bf3e0ca2a3d3516cd893c961986654
Sergey B. <sergey3bv@gmail.com>
2025-07-09 12:20:41 +0300

    feat: spaces routes were added

--- apps/api/src/routes/v1/index.ts
+++ apps/api/src/routes/v1/index.ts
@@ -2,8 +2,10 @@
 import { FastifyInstance } from 'fastify';
 import proposalsRoutes from './proposals';
 import dashboardRoutes from './dashboard';
+import spacesRoutes from './spaces';
 
 export default async function v1Routes(app: FastifyInstance) {
   app.register(proposalsRoutes, { prefix: '/proposals' });
   app.register(dashboardRoutes, { prefix: '/dashboard' });
+  app.register(spacesRoutes, { prefix: '/spaces' });
 }
