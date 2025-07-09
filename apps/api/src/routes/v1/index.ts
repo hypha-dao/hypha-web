c8b4d551def9702da0d7da69a190f7f0fa781336
Sergey B. <sergey3bv@gmail.com>
2025-07-09 11:54:36 +0300

    feat: dashboard routes were added

--- apps/api/src/routes/v1/index.ts
+++ apps/api/src/routes/v1/index.ts
@@ -1,7 +1,9 @@
 // apps/api/src/routes/v1/index.ts
 import { FastifyInstance } from 'fastify';
 import proposalsRoutes from './proposals';
+import dashboardRoutes from './dashboard';
 
 export default async function v1Routes(app: FastifyInstance) {
   app.register(proposalsRoutes, { prefix: '/proposals' });
+  app.register(dashboardRoutes, { prefix: '/dashboard' });
 }
