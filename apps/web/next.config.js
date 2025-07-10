e5a762a3f0210cee420143974e6197662339171a
Sergey B. <sergey3bv@gmail.com>
2025-07-10 16:24:37 +0300

    feat: hypha.earth was added as a remote

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -87,6 +87,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'hypha.earth',
+      },
     ],
   },
 };
