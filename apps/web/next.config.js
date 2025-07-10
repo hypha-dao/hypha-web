4c8af11485a6c229a9e63fbe22c86794ca2e3b24
Sergey B. <sergey3bv@gmail.com>
2025-07-10 15:51:53 +0300

    feat: x-frame-options header was added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/:path((?!api).*)*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: true,
+  },
   images: {
     remotePatterns: [
       {
