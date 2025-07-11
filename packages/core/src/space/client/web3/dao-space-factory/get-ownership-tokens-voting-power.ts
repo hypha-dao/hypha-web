92915a6b9aa1834dbaaa1f4e3859fd4d331762b3
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-11 16:34:46 +0300

    feat(#1044): implemented logic for disable 1t1v button

---
+++ packages/core/src/space/client/web3/dao-space-factory/get-ownership-tokens-voting-power.ts
@@ -0,0 +1,21 @@
+import {
+  ownershipTokenVotingPowerImplementationAbi,
+  ownershipTokenVotingPowerImplementationAddress,
+} from '@core/generated';
+
+export const getOwnershipTokensVotingPower = ({
+  spaceId,
+  chain = 8453,
+}: {
+  spaceId: bigint;
+  chain?: keyof typeof ownershipTokenVotingPowerImplementationAddress;
+}) => {
+  const address = ownershipTokenVotingPowerImplementationAddress[chain];
+
+  return {
+    address,
+    abi: ownershipTokenVotingPowerImplementationAbi,
+    functionName: 'spaceTokens',
+    args: [spaceId],
+  } as const;
+};
