539ca6ef4795397657ec6d8c657a4bb76e324f81
Sergey B. <sergey3bv@gmail.com>
2025-07-11 12:51:59 +0300

    chore: connect sources were moved to a config file

--- apps/web/src/lib/middleware/next.ts
+++ apps/web/src/lib/middleware/next.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { NextMiddlewareFunction, NextMiddlewareContext } from './types';
 import { IMAGE_HOSTS } from '../../config/image-hosts';
+import { CONNECT_SOURCES } from '../../config/connect-sources';
 
 /**
  * A utility type for Next.js middleware chain
@@ -159,7 +160,7 @@ export function cspMiddleware(): NextMiddlewareFunction {
         "frame-ancestors 'none'",
         'child-src https://auth.privy.io https://verify.walletconnect.com https verify.walletconnect.org',
         'frame-src https://auth.privy.io https://verify.walletconnect.com https verify.walletconnect.org https://challenges.cloudflare.com',
-        "connect-src 'self' https://auth.privy.io wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com",
+        `connect-src 'self' ${CONNECT_SOURCES.join(' ')}`,
         "worker-src 'self'",
         "manifest-src 'self'",
       ].join(';') + ';';
