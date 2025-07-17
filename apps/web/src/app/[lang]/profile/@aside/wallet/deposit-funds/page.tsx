a0136b6d583c88c35d0f155ebd72df88104fa554
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-17 15:03:10 +0300

    feat(#1061): added back button and read person address

--- apps/web/src/app/[lang]/profile/@aside/wallet/deposit-funds/page.tsx
+++ apps/web/src/app/[lang]/profile/@aside/wallet/deposit-funds/page.tsx
@@ -2,19 +2,25 @@
 
 import { SidePanel } from '@hypha-platform/epics';
 import React from 'react';
-import { ButtonClose } from '@hypha-platform/epics';
+import { ButtonClose, ButtonBack } from '@hypha-platform/epics';
 import { useParams } from 'next/navigation';
 import { PersonDepositFunds } from '@hypha-platform/epics';
+import { useMe } from '@hypha-platform/core/client';
 
 export default function ProfileWalletDepositFunds() {
   const { lang } = useParams();
+  const { person } = useMe();
   return (
     <SidePanel>
       <div className="flex flex-col gap-5">
         <div className="flex gap-5 justify-end items-center">
+          <ButtonBack
+            label="Back to wallet"
+            backUrl={`/${lang}/profile/wallet`}
+          />
           <ButtonClose closeUrl={`/${lang}/profile`} />
         </div>
-        <PersonDepositFunds />
+        <PersonDepositFunds personAddress={person?.address as `0x${string}`} />
       </div>
     </SidePanel>
   );
