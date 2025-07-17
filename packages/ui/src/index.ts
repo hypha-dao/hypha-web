c7a1d4c29f28a615f706db62c3d97ae5682cb4b0
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-17 12:39:51 -0300

    fix imports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -10,6 +10,7 @@ export * from './upload/lead-image';
 export * from './avatar';
 export * from './badge';
 export * from './button';
+export * from './button-nav-item';
 export * from './card';
 export * from './carousel';
 export * from './chips';
--- packages/ui/src/organisms/index.ts
+++ packages/ui/src/organisms/index.ts
@@ -1,7 +1,5 @@
 export * from './amount';
-export * from './dialog';
 export * from './footer';
-export * from './navigation';
 export * from './section-filter';
 export * from './section-load-more';
 export * from './tabs';
