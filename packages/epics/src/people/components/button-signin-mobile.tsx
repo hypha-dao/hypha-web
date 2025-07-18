7cdd93e2b9af6783e324aaa5376d8d8f286b256e
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-18 13:45:57 -0300

    fix: return to app link

--- packages/epics/src/people/components/button-signin-mobile.tsx
+++ packages/epics/src/people/components/button-signin-mobile.tsx
@@ -149,7 +149,7 @@ export const ButtonSigninMobile = ({
       )}
       {error && <p className="text-error-11">{error}</p>}
       <Button variant="link" asChild>
-        <Link href={`${mobileRedirectUrl}${token ? `?token=${token}` : ''}`}>
+        <Link href={token ? `${mobileRedirectUrl}?token=${token}` : mobileRedirectUrl}>
           Return to the app
         </Link>
       </Button>
