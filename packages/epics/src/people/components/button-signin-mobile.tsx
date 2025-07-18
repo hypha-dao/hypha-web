98b813308ad3aad0504b0211766396a1e1f27cbc
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-18 12:14:16 -0300

    chore: signout flow on mobile

--- packages/epics/src/people/components/button-signin-mobile.tsx
+++ packages/epics/src/people/components/button-signin-mobile.tsx
@@ -7,6 +7,7 @@ import {
   Button,
 } from '@hypha-platform/ui';
 import { Loader2 } from 'lucide-react';
+import Link from 'next/link';
 
 type ButtonSigninMobileProps = {
   useAuthentication: UseAuthentication;
@@ -37,25 +38,34 @@ export const ButtonSigninMobile = ({
     setLoggingIn,
     user,
     isLoading: isAuthLoading,
-    getAccessToken
+    getAccessToken,
+    isModalOpen,
   } = useAuthentication();
   const { person, isLoading: isPersonLoading } = useMe();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isAuthLoading || isPersonLoading) {
+    setLoading(false);
+    // Modal open and close before getting the token and after reloading on signin
+    // It should redirect if user exists and model was opened
+    if (isModalOpen) {
+      setShouldRedirect(true);
+    } 
+    if (isAuthLoading || isPersonLoading || isModalOpen) {
       setError(null);
       setLoading(true);
       return;
     }
+
     if(!isAuthenticated) {
       return
     }
 
     if(!mobileRedirectUrl) {
       setError('Mobile redirect URL is not configured.');
-      setLoading(false);
       return;
     }
 
@@ -63,18 +73,19 @@ export const ButtonSigninMobile = ({
       try {
         if (user) {
           if (person) {
-            setLoading(true);
             if (isErrorUser(person)) {
               setError(person.error);
-              setLoading(false);
-            } else if (!isLoggingIn){
-              const token = await getAccessToken();
-              if (!token) {
+            } else {
+              const accessToken = await getAccessToken();
+              if (!accessToken) {
+                setToken(null);
                 setError('Failed to retrieve access token.');
-                setLoading(false);
                 return;
               }
-              window.location.href = `${mobileRedirectUrl}?token=${token}`;
+              setToken(accessToken);
+              if (shouldRedirect) {
+                window.location.href = `${mobileRedirectUrl}?token=${accessToken}`;
+              }
             }
           } else {
             logout(false);
@@ -98,21 +109,53 @@ export const ButtonSigninMobile = ({
     mobileRedirectUrl,
     isLoggingIn,
     setLoggingIn,
+    isModalOpen,
   ]);
 
+  const handleSignOut = async () => {
+    setShouldRedirect(false);
+    setToken(null);
+    setError(null);
+    logout(false);
+  }
+
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-2">
-      <Button
-        onClick={login}
-        className="flex gap-2"
-        disabled={loading}
-      >
-        {
-          loading && <Loader2 className="animate-spin w-4 h-4" />
-        }
-        <span>Sign in</span>
-      </Button>
+    <div className="w-full flex flex-col items-center justify-center gap-4">
+      {(!isAuthenticated || loading || error) &&
+        <Button
+          onClick={() => login()}
+          className="flex gap-2"
+          disabled={loading}
+        >
+          {
+            loading && <Loader2 className="animate-spin w-4 h-4" />
+          }
+          <span>Sign in</span>
+        </Button>
+      }
+      {isAuthenticated && !loading && !error &&
+        <Button
+          variant="outline"
+          colorVariant="error"
+          onClick={handleSignOut}
+          className="flex gap-2"
+        >
+          <span>Sign out</span>
+        </Button>
+      }
+      {isAuthenticated && loading && shouldRedirect && <p className="text-muted-foreground">Redirecting...</p>}
+      {isAuthenticated && !loading && !error && person && (   
+        <p className="text-muted-foreground">
+          You are signed in as {person.name}
+        </p>
+      )}
       {error && <p className="text-error-11">{error}</p>}
+      <Button
+        variant="link"
+        asChild
+      >
+        <Link href={`${mobileRedirectUrl}${token ? `?token=${token}` : ''}`}>Return to the app</Link>
+      </Button>
     </div>
   );
 };
