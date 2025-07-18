dbceafdaa5805334e8598337bd59b7a5cbc2d0e0
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-18 12:22:14 -0300

    chore: check mobile user agent for sigin page

--- apps/web/src/app/(mobile)/signin/page.tsx
+++ apps/web/src/app/(mobile)/signin/page.tsx
@@ -1,8 +1,21 @@
 import { useAuthentication } from '@hypha-platform/authentication';
 import { ButtonSigninMobile } from '@hypha-platform/epics';
 import { useMe } from '@hypha-platform/core/client';
+import { notFound } from 'next/navigation';
+import { headers } from 'next/headers';
 
 export default async function Index() {
+  const headersList = await headers();
+  const ua = headersList.get('user-agent') || '';
+  const hyphaUserAgent = process.env.NEXT_PUBLIC_USER_AGENT || '';
+
+  // If mobile user agent is set, it checks if the user is using Hypha app
+  const isHyphaApp = !hyphaUserAgent || ua.includes(hyphaUserAgent);
+
+  if (!isHyphaApp) {
+    notFound();
+  }
+
   return (
     <div className="flex items-center h-full">
       <ButtonSigninMobile
