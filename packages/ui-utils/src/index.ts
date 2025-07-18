ff1472346dbf5f1a9b8417630996afb047de7c2e
Dmitry E <dsanichweb@gmail.com>
2025-07-18 13:22:51 +0300

    feat(#1089): added support absolute links in space settings

--- packages/ui-utils/src/index.ts
+++ packages/ui-utils/src/index.ts
@@ -1,5 +1,6 @@
 export * from './cn';
 export * from './copyToClipboard';
+export * from './isAbsoluteUrl';
 export * from './strip-markdown';
 export * from './formatCurrencyValue';
 export * from './formatDate';
