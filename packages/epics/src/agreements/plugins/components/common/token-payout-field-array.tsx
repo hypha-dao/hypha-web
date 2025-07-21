5638cb1b0028be981c3bcb998ac32fd79ef772e5
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-21 19:55:55 -0300

    fix token selector input ui

--- packages/epics/src/agreements/plugins/components/common/token-payout-field-array.tsx
+++ packages/epics/src/agreements/plugins/components/common/token-payout-field-array.tsx
@@ -37,33 +37,54 @@ export const TokenPayoutFieldArray = ({
     append({ amount: '', token: '' as `0x${string}` });
   };
 
+  const handleDeleteField = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    index: number,
+  ) => {
+    e.preventDefault();
+    if (fields.length > 1) {
+      remove(index);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2 w-full">
-      {fields.map((field, index) => (
-        <div key={field.id} className="flex items-end gap-2">
-          <div className="flex-1">
-            <FormField
-              control={control}
-              name={`${name}.${index}`}
-              render={({ field: { value, onChange } }) => (
-                <FormItem>
-                  <FormControl>
-                    <TokenPayoutField
-                      value={value}
-                      onChange={onChange}
-                      tokens={tokens}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button variant="ghost" onClick={() => remove(index)}>
-            <Cross2Icon />
-          </Button>
+      <div className="flex flex-col gap-4 md:gap-8 md:flex-row md:items-start w-full">
+        <label className="text-2 text-neutral-11 whitespace-nowrap md:min-w-max items-center md:pt-1">
+          Payment Request
+        </label>
+        <div className="flex flex-col gap-2">
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex md:justify-end gap-2">
+              <div className="">
+                <FormField
+                  control={control}
+                  name={`${name}.${index}`}
+                  render={({ field: { value, onChange } }) => (
+                    <FormItem>
+                      <FormControl>
+                        <TokenPayoutField
+                          value={value}
+                          onChange={onChange}
+                          tokens={tokens}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button
+                variant="ghost"
+                onClick={(ev) => handleDeleteField(ev, index)}
+                className="px-2 md:px-3"
+              >
+                <Cross2Icon />
+              </Button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
       <div className="flex justify-end w-full">
         <Button className="w-fit" onClick={handleAddField} variant="ghost">
           <PlusIcon />
