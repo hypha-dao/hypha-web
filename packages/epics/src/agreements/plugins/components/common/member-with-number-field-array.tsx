17f9e3bfe85ee531b729a454f3545ab6844879d0
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-21 20:43:05 -0300

    fix ui 1 voice 1 vote

--- packages/epics/src/agreements/plugins/components/common/member-with-number-field-array.tsx
+++ packages/epics/src/agreements/plugins/components/common/member-with-number-field-array.tsx
@@ -32,42 +32,53 @@ export const MemberWithNumberFieldFieldArray = ({
     e.preventDefault();
     append({ member: null, number: '' });
   };
+  const handleDeleteField = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    index: number,
+  ) => {
+    e.preventDefault();
+    if (fields.length > 1) {
+      remove(index);
+    }
+  };
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-4">
       <Separator />
-      <Text className="text-2 text-neutral-11">Initial voice allocation</Text>
-      <div className="flex flex-col gap-2 w-full items-end">
-        {fields.map((field, index) => (
-          <div key={field.id} className="flex items-center gap-2">
-            <div className="flex-1">
-              <FormField
-                control={control}
-                name={`${name}.${index}`}
-                render={({ field: { value, onChange } }) => (
-                  <FormItem>
-                    <FormControl>
-                      <MemberWithNumberField
-                        members={members}
-                        value={value}
-                        onChange={onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+      <div className="flex flex-col md:flex-row gap-4">
+        <Text className="pt-1 text-2 text-neutral-11 text-nowrap">Initial voice allocation</Text>
+        <div className="flex flex-col gap-2">
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex items-center gap-2">
+              <div className="flex-1">
+                <FormField
+                  control={control}
+                  name={`${name}.${index}`}
+                  render={({ field: { value, onChange } }) => (
+                    <FormItem>
+                      <FormControl>
+                        <MemberWithNumberField
+                          members={members}
+                          value={value}
+                          onChange={onChange}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button variant="ghost" onClick={(ev) => handleDeleteField(ev, index)}>
+                <Cross2Icon />
+              </Button>
             </div>
-            <Button variant="ghost" onClick={() => remove(index)}>
-              <Cross2Icon />
+          ))}
+          <div className="flex justify-end w-full">
+            <Button className="w-fit" onClick={handleAddField} variant="ghost">
+              <PlusIcon />
+              Add
             </Button>
           </div>
-        ))}
-        <div className="flex justify-end w-full">
-          <Button className="w-fit" onClick={handleAddField} variant="ghost">
-            <PlusIcon />
-            Add
-          </Button>
         </div>
       </div>
     </div>
