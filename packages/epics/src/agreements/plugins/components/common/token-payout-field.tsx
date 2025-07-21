367ba9e1bb9653acf8b2150330e72947eb16b14c
Carlos Zico Montibeller <carlosmontibeller@think-it.io>
2025-07-21 19:55:55 -0300

    fix token selector input ui

--- packages/epics/src/agreements/plugins/components/common/token-payout-field.tsx
+++ packages/epics/src/agreements/plugins/components/common/token-payout-field.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuTrigger,
   Input,
   Image,
+  Button,
 } from '@hypha-platform/ui';
 
 interface Token {
@@ -41,61 +42,57 @@ export const TokenPayoutField = ({
   };
 
   return (
-    <div className="flex justify-between w-full">
-      <label className="text-2 text-neutral-11 flex items-center">
-        Payment Request
-      </label>
-      <div className="flex gap-2 items-center">
-        <Input
-          value={value.amount}
-          type="number"
-          placeholder="Type an amount"
-          onChange={(e) => handleAmountChange(e.target.value)}
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div className="flex justify-between items-center gap-2 min-w-[140px] px-4 py-2 border border-neutral-7 rounded-md hover:bg-neutral-3 cursor-pointer">
-              <div className="flex items-center gap-2">
-                {selectedToken ? (
-                  <>
-                    <Image
-                      src={selectedToken.icon}
-                      width={20}
-                      height={20}
-                      alt={selectedToken.symbol}
-                    />
-                    <span className="text-2 text-neutral-11">
-                      {selectedToken.symbol}
-                    </span>
-                  </>
-                ) : (
-                  <span className="text-2 text-neutral-11 text-nowrap">
-                    Select a token
-                  </span>
-                )}
-              </div>
-              <ChevronDownIcon />
+    <div className="flex flex-row md:justify-end gap-4 items-end">
+      <Input
+        value={value.amount}
+        type="number"
+        placeholder="Type an amount"
+        onChange={(e) => handleAmountChange(e.target.value)}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            colorVariant="neutral"
+            role="combobox"
+            className="w-full flex-1 text-2 md:w-72 justify-between py-2 font-normal"
+          >
+            <div className="flex items-center gap-2 w-24">
+              {selectedToken ? (
+                <>
+                  <Image
+                    src={selectedToken.icon}
+                    width={20}
+                    height={20}
+                    alt={selectedToken.symbol}
+                  />
+                  <span className="text-2">{selectedToken.symbol}</span>
+                </>
+              ) : (
+                <span className="text-2 text-nowrap">Select a token</span>
+              )}
             </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {tokens.map((token) => (
-              <DropdownMenuItem
-                key={`${token.address}_${token.symbol}`}
-                onSelect={() => handleTokenChange(token)}
-              >
-                <Image
-                  src={token.icon}
-                  width={24}
-                  height={24}
-                  alt={token.symbol}
-                  className="mr-2 object-cover"
-                />
-                <span className="text-2 text-neutral-11">{token.symbol}</span>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+            <ChevronDownIcon className="size-2" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {tokens.map((token) => (
+            <DropdownMenuItem
+              key={`${token.address}_${token.symbol}`}
+              onSelect={() => handleTokenChange(token)}
+            >
+              <Image
+                src={token.icon}
+                width={24}
+                height={24}
+                alt={token.symbol}
+                className="mr-2 object-cover"
+              />
+              <span className="text-2 text-neutral-11">{token.symbol}</span>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
