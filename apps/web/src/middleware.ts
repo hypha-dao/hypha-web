a464873cd105fe48032e9cce88cd62d284fe9d35
Philipp Litzenberger <philipp@bendcircular.com>
2025-07-22 10:21:19 +0200

    chore: dedicated match

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,10 +1,8 @@
 export { middleware } from '@hypha-platform/i18n';
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    '/signin',
-    '/sample',
+    '/((?!sample|signin))',
     '/((?!api|placeholder|.well-known|_next/static|_next/image|favicon.ico).*)',
   ],
 };
