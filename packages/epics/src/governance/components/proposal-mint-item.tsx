99fa1d1a60b0fc9258b2f8db71e62aeb9fb78a5e
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-22 15:33:39 +0300

    feat(#1137): fixed token number on proposal details

--- packages/epics/src/governance/components/proposal-mint-item.tsx
+++ packages/epics/src/governance/components/proposal-mint-item.tsx
@@ -11,7 +11,7 @@ export const ProposalMintItem = ({ member, number }: ProposalMintItemProps) => {
     <div className="flex flex-col gap-5">
       <div className="flex justify-between items-center">
         <div className="text-1 w-full">{member}</div>
-        <div className="text-1">{originalNumber}</div>
+        <div className="text-1">{Number(originalNumber).toFixed(2)}</div>
       </div>
     </div>
   );
