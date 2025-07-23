38978c2c1f7cd1a82873d397c8086532f2883a0f
Dmitry E <dsanichweb@gmail.com>
2025-07-23 11:59:11 +0300

    feat(#1150): updated path

--- packages/epics/src/treasury/components/assets/user-assets-section.tsx
+++ packages/epics/src/treasury/components/assets/user-assets-section.tsx
@@ -32,7 +32,7 @@ export const UserAssetsSection: FC<UserAssetsSectionProps> = ({
     <div className="flex flex-col w-full justify-center items-center gap-4">
       <SectionFilter count={totalBalance || 0} label="Balance">
         <Button asChild className="ml-2">
-          <Link href={`${basePath}/wallet`} scroll={false}>
+          <Link href={`${basePath}/actions`} scroll={false}>
             Actions
           </Link>
         </Button>
