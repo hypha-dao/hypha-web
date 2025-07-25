e1e28d2ee519d64f6aa9a78174ec8c20ba4d10db
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-25 15:04:59 +0300

    feat(#1193): added alchemy-client instance

--- packages/core/src/common/server/index.ts
+++ packages/core/src/common/server/index.ts
@@ -8,3 +8,4 @@ export * from './moralis-client';
 export * from './get-token-price';
 export * from './get-transfers-by-address';
 export * from './get-wallet-token-balances-price';
+export * from './alchemy-client';
