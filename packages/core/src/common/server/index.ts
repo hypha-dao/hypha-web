debf4974d820241685ba07c8e131b2721a5cfb00
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-07-28 14:35:56 +0300

    feat(#1193): code cleared from Moralis

--- packages/core/src/common/server/index.ts
+++ packages/core/src/common/server/index.ts
@@ -4,7 +4,6 @@ export * from './get-db';
 export * from './pagination';
 export * from './order';
 
-export * from './moralis-client';
 export * from './get-token-price';
 export * from './get-transfers-by-address';
 export * from './get-token-balances-by-address';
