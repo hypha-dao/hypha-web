047762b42fe6357b2b40132c9d9d09410066ca47
Sergey B. <sergey3bv@gmail.com>
2025-07-28 13:17:35 +0300

    feat: getSpaceDetails was rewritten to return actual details

--- packages/core/src/space/client/web3/dao-space-factory/get-space-details.ts
+++ packages/core/src/space/client/web3/dao-space-factory/get-space-details.ts
@@ -2,20 +2,85 @@ import {
   daoSpaceFactoryImplementationAbi,
   daoSpaceFactoryImplementationAddress,
 } from '@hypha-platform/core/generated';
+import { publicClient } from '@hypha-platform/core/client';
 
-export const getSpaceDetails = ({
-  spaceId,
+export interface SpaceDetails {
+  unity: bigint;
+  quorum: bigint;
+  votingPowerSource: bigint;
+  tokenAddresses: readonly `0x${string}`[];
+  members: readonly `0x${string}`[];
+  exitMethod: bigint;
+  joinMethod: bigint;
+  createdAt: bigint;
+  creator: `0x${string}`;
+  executor: `0x${string}`;
+}
+
+export const getSpaceDetails = async ({
+  spaceIds: spaceIds,
   chain = 8453,
 }: {
-  spaceId: bigint;
+  spaceIds: bigint[];
   chain?: keyof typeof daoSpaceFactoryImplementationAddress;
-}) => {
+}): Promise<Map<bigint, SpaceDetails>> => {
+  if (spaceIds.length === 0) return new Map();
+
   const address = daoSpaceFactoryImplementationAddress[chain];
 
-  return {
-    address,
-    abi: daoSpaceFactoryImplementationAbi,
-    functionName: 'getSpaceDetails',
-    args: [spaceId],
-  } as const;
+  const params = spaceIds.map(
+    (id) =>
+      ({
+        address,
+        abi: daoSpaceFactoryImplementationAbi,
+        functionName: 'getSpaceDetails',
+        args: [id],
+      } as const),
+  );
+
+  try {
+    const response = await publicClient.multicall({
+      allowFailure: false,
+      blockTag: 'safe',
+      contracts: params,
+    });
+    if (response.length !== spaceIds.length) {
+      throw new Error('Response length is different from input length');
+    }
+
+    const details = response.map((res) => {
+      const [
+        unity,
+        quorum,
+        votingPowerSource,
+        tokenAddresses,
+        members,
+        exitMethod,
+        joinMethod,
+        createdAt,
+        creator,
+        executor,
+      ] = res;
+
+      return {
+        unity,
+        quorum,
+        votingPowerSource,
+        tokenAddresses,
+        members,
+        exitMethod,
+        joinMethod,
+        createdAt,
+        creator,
+        executor,
+      };
+    });
+
+    return new Map<bigint, SpaceDetails>(
+      // Arrays will have the same length at this point
+      spaceIds.map((id, i) => [id, details[i] as SpaceDetails]),
+    );
+  } catch (e) {
+    throw new Error(`Failed to get space details: ${e}`);
+  }
 };
