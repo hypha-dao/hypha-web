b2cb087e3c5541ef9cd44374592ece092789eb9e
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-08-07 09:58:10 +0300

    feat(#1031): implemented authorization in tokens route

--- apps/web/src/app/api/v1/tokens/route.ts
+++ apps/web/src/app/api/v1/tokens/route.ts
@@ -5,9 +5,11 @@ import { db } from '@hypha-platform/storage-postgres';
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const search = searchParams.get('search') || undefined;
+  const authToken = request.headers.get('Authorization')?.split(' ')[1] || '';
 
-  // TODO: implement authorization
-  // const authToken = request.headers.get('Authorization')?.split(' ')[1] || '';
+  if (!authToken) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
 
   try {
     const tokens = await findAllTokens({ db }, { search });
