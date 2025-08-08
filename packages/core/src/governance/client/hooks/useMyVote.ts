f66af1295507ef591276350ae90d37b59120a4e0
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-08-08 14:14:26 +0300

    feat(#1240): code improvements

--- packages/core/src/governance/client/hooks/useMyVote.ts
+++ packages/core/src/governance/client/hooks/useMyVote.ts
@@ -4,13 +4,17 @@ import { useMe, useProposalVoters } from '@hypha-platform/core/client';
 
 export const useMyVote = (documentSlug?: string) => {
   const { person } = useMe();
-  const { voters, isLoading, error, mutate } = useProposalVoters(documentSlug);
+  const { voters, isLoading, error, mutate } = useProposalVoters(
+    person?.address ? documentSlug : undefined,
+  );
 
   if (!person?.address || !voters || isLoading) {
     return { myVote: null, isLoading, error, mutate };
   }
 
-  const myVoteData = voters.find((voter) => voter.address === person.address);
+  const myVoteData = voters.find(
+    (v) => v.address.toLowerCase() === person.address?.toLowerCase(),
+  );
 
   const myVote = myVoteData ? myVoteData.vote : null;
 
