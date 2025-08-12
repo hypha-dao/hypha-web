6f0c76d7e5d97e40aab7dfa4a456f1111521e724
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-08-12 13:22:14 +0300

    feat(#1278): improved voting panel UX

--- apps/web/src/app/[lang]/dho/[id]/@aside/governance/proposal/[documentSlug]/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/governance/proposal/[documentSlug]/page.tsx
@@ -12,6 +12,8 @@ import { useSpaceBySlug } from '@hypha-platform/core/client';
 import { useDbTokens } from '@web/hooks/use-db-tokens';
 import { useJwt } from '@hypha-platform/core/client';
 import { useProposalDetailsWeb3Rpc } from '@hypha-platform/core/client';
+import { LoadingBackdrop } from '@hypha-platform/ui';
+import { useState } from 'react';
 
 export default function Agreements() {
   const { jwt: authToken } = useJwt();
@@ -38,33 +40,71 @@ export default function Agreements() {
   });
   const { tokens } = useDbTokens();
 
+  const [isBackdropVisible, setIsBackdropVisible] = useState(false);
+  const handleOnAccept = async () => {
+    try {
+      await handleAccept();
+      await mutate();
+      await update();
+    } catch (err) {
+      console.debug(err);
+      setIsBackdropVisible(false);
+    }
+  };
+
+  const handleOnReject = async () => {
+    try {
+      await handleReject();
+      await mutate();
+      await update();
+    } catch (err) {
+      console.debug(err);
+      setIsBackdropVisible(false);
+    }
+  };
+
+  const handleOnCheckProposalExpiration = async () => {
+    try {
+      await handleCheckProposalExpiration();
+      await mutate();
+      await update();
+    } catch (err) {
+      console.debug(err);
+      setIsBackdropVisible(false);
+    }
+  };
+
   return (
     <SidePanel>
-      <ProposalDetail
-        closeUrl={getDhoPathGovernance(lang as Locale, id as string)}
-        onAccept={handleAccept}
-        onReject={handleReject}
-        onCheckProposalExpiration={handleCheckProposalExpiration}
-        updateProposalData={mutate}
-        updateProposalsList={update}
-        isVoting={isVoting}
-        content={document?.description}
-        creator={{
-          avatar: document?.creator?.avatarUrl || '',
-          name: document?.creator?.name || '',
-          surname: document?.creator?.surname || '',
-        }}
-        title={document?.title}
-        status={document?.state}
-        isLoading={isLoading}
-        leadImage={document?.leadImage}
-        attachments={document?.attachments}
-        proposalId={document?.web3ProposalId}
-        spaceSlug={space?.slug || ''}
-        label={document?.label || ''}
-        documentSlug={documentSlug}
-        dbTokens={tokens || []}
-      />
+      <LoadingBackdrop
+        isLoading={isBackdropVisible || isLoading}
+        message={<span>Please wait...</span>}
+        className="-m-4 lg:-m-7"
+      >
+        <ProposalDetail
+          closeUrl={getDhoPathGovernance(lang as Locale, id as string)}
+          onAccept={handleOnAccept}
+          onReject={handleOnReject}
+          onCheckProposalExpiration={handleOnCheckProposalExpiration}
+          isVoting={isVoting}
+          content={document?.description}
+          creator={{
+            avatar: document?.creator?.avatarUrl || '',
+            name: document?.creator?.name || '',
+            surname: document?.creator?.surname || '',
+          }}
+          title={document?.title}
+          status={document?.state}
+          isLoading={isLoading}
+          leadImage={document?.leadImage}
+          attachments={document?.attachments}
+          proposalId={document?.web3ProposalId}
+          spaceSlug={space?.slug || ''}
+          label={document?.label || ''}
+          documentSlug={documentSlug}
+          dbTokens={tokens || []}
+        />
+      </LoadingBackdrop>
     </SidePanel>
   );
 }
--- packages/epics/src/proposals/components/proposal-detail.tsx
+++ packages/epics/src/proposals/components/proposal-detail.tsx
@@ -23,16 +23,11 @@ import {
 } from '../../governance';
 import { MarkdownSuspense } from '@hypha-platform/ui/server';
 import { ButtonClose } from '@hypha-platform/epics';
-import { LoadingBackdrop } from '@hypha-platform/ui/server';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 
 type ProposalDetailProps = ProposalHeadProps & {
   onAccept: () => void;
   onReject: () => void;
   onCheckProposalExpiration: () => void;
-  updateProposalData: () => void;
-  updateProposalsList: () => void;
   isVoting?: boolean;
   content?: string;
   closeUrl: string;
@@ -59,165 +54,108 @@ export const ProposalDetail = ({
   leadImage,
   attachments,
   proposalId,
-  updateProposalData,
-  updateProposalsList,
   spaceSlug,
   isVoting,
   label,
   documentSlug,
   dbTokens,
 }: ProposalDetailProps) => {
-  const router = useRouter();
   const { proposalDetails } = useProposalDetailsWeb3Rpc({
     proposalId: proposalId as number,
   });
-  const [isBackdropVisible, setIsBackdropVisible] = useState(false);
-
-  const showBackdrop = () => {
-    setIsBackdropVisible(true);
-    setTimeout(() => {
-      setIsBackdropVisible(false);
-      router.push(closeUrl);
-    }, 5000);
-  };
-
-  const handleOnAccept = () => {
-    try {
-      showBackdrop();
-      onAccept();
-      updateProposalData();
-      updateProposalsList();
-    } catch (err) {
-      console.debug(err);
-      setIsBackdropVisible(false);
-    }
-  };
-
-  const handleOnReject = () => {
-    try {
-      showBackdrop();
-      onReject();
-      updateProposalData();
-      updateProposalsList();
-    } catch (err) {
-      console.debug(err);
-      setIsBackdropVisible(false);
-    }
-  };
-
-  const handleOnCheckProposalExpiration = () => {
-    try {
-      showBackdrop();
-      onCheckProposalExpiration();
-      updateProposalData();
-      updateProposalsList();
-    } catch (err) {
-      console.debug(err);
-      setIsBackdropVisible(false);
-    }
-  };
 
   return (
-    <LoadingBackdrop
-      isLoading={isBackdropVisible || isLoading}
-      message={<span>Please wait...</span>}
-    >
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5 justify-between">
-          <ProposalHead
-            creator={creator}
-            title={title}
-            commitment={commitment}
-            status={status}
-            isLoading={isLoading}
-            label={label}
-            createDate={formatDate(proposalDetails?.startTime ?? new Date())}
-          />
-          <ButtonClose closeUrl={closeUrl} />
-        </div>
-        <Separator />
-        <Skeleton
-          width="100%"
-          height="150px"
-          loading={isLoading}
-          className="rounded-lg"
-        >
-          <Image
-            height={150}
-            width={554}
-            className="w-full object-cover rounded-lg max-h-[150px]"
-            src={leadImage || DEFAULT_SPACE_LEAD_IMAGE}
-            alt={title ?? ''}
-          />
-        </Skeleton>
-        <MarkdownSuspense>{content}</MarkdownSuspense>
-        <AttachmentList attachments={attachments || []} />
-        {proposalDetails?.votingMethods.map((method, idx) => (
-          <ProposalVotingInfo
-            key={idx}
-            votingPowerSource={method.votingPowerSource}
-            unity={method.unity}
-            quorum={method.quorum}
-          />
-        ))}
-        {proposalDetails?.entryMethods.map((method, idx) => (
-          <ProposalEntryInfo key={idx} joinMethod={method.joinMethod} />
-        ))}
-        {proposalDetails?.tokenRequirements.map((method, idx) => (
-          <ProposalTokenRequirementsInfo
-            key={idx}
-            token={method.token}
-            amount={method.amount}
-            spaceSlug={spaceSlug}
-          />
-        ))}
-        {proposalDetails?.tokens.map((token, idx) => (
-          <ProposalTokenItem
-            key={idx}
-            name={token.name}
-            symbol={token.symbol}
-            initialSupply={token.maxSupply}
-            dbTokens={dbTokens}
-          />
-        ))}
-        {proposalDetails?.transfers.map((tx, idx) => (
-          <ProposalTransactionItem
-            key={idx}
-            recipient={tx?.recipient}
-            amount={tx?.rawAmount}
-            tokenAddress={tx?.token}
-            spaceSlug={spaceSlug}
-          />
-        ))}
-        {proposalDetails?.mintings.map((mint, idx) => (
-          <ProposalMintItem
-            key={idx}
-            member={mint.member}
-            number={mint.number}
-          />
-        ))}
-        <FormVoting
-          unity={proposalDetails?.unityPercentage || 0}
-          quorum={proposalDetails?.quorumPercentage || 0}
-          endTime={formatISO(new Date(proposalDetails?.endTime || new Date()))}
-          executed={proposalDetails?.executed}
-          expired={proposalDetails?.expired}
-          onAccept={handleOnAccept}
-          onReject={handleOnReject}
-          onCheckProposalExpiration={handleOnCheckProposalExpiration}
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-5 justify-between">
+        <ProposalHead
+          creator={creator}
+          title={title}
+          commitment={commitment}
+          status={status}
           isLoading={isLoading}
-          isVoting={isVoting}
-          documentSlug={documentSlug}
+          label={label}
+          createDate={formatDate(proposalDetails?.startTime ?? new Date())}
         />
-        {/* TODO: uncomment when comments support will be implemented */}
-        {/* <Separator />
-        <CommentsList
-          pagination={{
-            total: 0,
-          }}
-          comments={[]}
-        /> */}
+        <ButtonClose closeUrl={closeUrl} />
       </div>
-    </LoadingBackdrop>
+      <Separator />
+      <Skeleton
+        width="100%"
+        height="150px"
+        loading={isLoading}
+        className="rounded-lg"
+      >
+        <Image
+          height={150}
+          width={554}
+          className="w-full object-cover rounded-lg max-h-[150px]"
+          src={leadImage || DEFAULT_SPACE_LEAD_IMAGE}
+          alt={title ?? ''}
+        />
+      </Skeleton>
+      <MarkdownSuspense>{content}</MarkdownSuspense>
+      <AttachmentList attachments={attachments || []} />
+      {proposalDetails?.votingMethods.map((method, idx) => (
+        <ProposalVotingInfo
+          key={idx}
+          votingPowerSource={method.votingPowerSource}
+          unity={method.unity}
+          quorum={method.quorum}
+        />
+      ))}
+      {proposalDetails?.entryMethods.map((method, idx) => (
+        <ProposalEntryInfo key={idx} joinMethod={method.joinMethod} />
+      ))}
+      {proposalDetails?.tokenRequirements.map((method, idx) => (
+        <ProposalTokenRequirementsInfo
+          key={idx}
+          token={method.token}
+          amount={method.amount}
+          spaceSlug={spaceSlug}
+        />
+      ))}
+      {proposalDetails?.tokens.map((token, idx) => (
+        <ProposalTokenItem
+          key={idx}
+          name={token.name}
+          symbol={token.symbol}
+          initialSupply={token.maxSupply}
+          dbTokens={dbTokens}
+        />
+      ))}
+      {proposalDetails?.transfers.map((tx, idx) => (
+        <ProposalTransactionItem
+          key={idx}
+          recipient={tx?.recipient}
+          amount={tx?.rawAmount}
+          tokenAddress={tx?.token}
+          spaceSlug={spaceSlug}
+        />
+      ))}
+      {proposalDetails?.mintings.map((mint, idx) => (
+        <ProposalMintItem key={idx} member={mint.member} number={mint.number} />
+      ))}
+      <FormVoting
+        unity={proposalDetails?.unityPercentage || 0}
+        quorum={proposalDetails?.quorumPercentage || 0}
+        endTime={formatISO(new Date(proposalDetails?.endTime || new Date()))}
+        executed={proposalDetails?.executed}
+        expired={proposalDetails?.expired}
+        onAccept={onAccept}
+        onReject={onReject}
+        onCheckProposalExpiration={onCheckProposalExpiration}
+        isLoading={isLoading}
+        isVoting={isVoting}
+        documentSlug={documentSlug}
+      />
+      {/* TODO: uncomment when comments support will be implemented */}
+      {/* <Separator />
+      <CommentsList
+        pagination={{
+          total: 0,
+        }}
+        comments={[]}
+      /> */}
+    </div>
   );
 };
