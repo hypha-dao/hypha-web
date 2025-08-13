3e3fda24ecfc8aceabb8c621bea9e188e71fdfcf
Dmitry E <dsanichweb@gmail.com>
2025-08-13 10:44:21 +0300

    feat(#1287): formatting

--- packages/ui-utils/src/get-duration.ts
+++ packages/ui-utils/src/get-duration.ts
@@ -2,7 +2,9 @@ const DAY_DURATION_IN_SECS: bigint = 86_400n;
 
 export const getDuration = (days: number): bigint => {
   if (!Number.isInteger(days) || days < 0) {
-    throw new Error('getDuration expects a non-negative integer number of days');
+    throw new Error(
+      'getDuration expects a non-negative integer number of days',
+    );
   }
   return BigInt(days) * DAY_DURATION_IN_SECS;
 };
