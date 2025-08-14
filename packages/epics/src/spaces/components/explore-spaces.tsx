47271a4bcb6aa3c7443341bae12dbfa650e2432a
Dmitry E <dsanichweb@gmail.com>
2025-08-14 15:18:59 +0300

    feat(#1223): updated some styles

--- packages/epics/src/spaces/components/explore-spaces.tsx
+++ packages/epics/src/spaces/components/explore-spaces.tsx
@@ -21,6 +21,28 @@ const categoriesIntersected = (
   categories2: Category[],
 ) => categories1.some((category) => categories2.includes(category));
 
+const ChooseCategoriesComboBox = ({
+  categories,
+}: {
+  categories: Category[];
+}) => {
+  return (
+    <Text className="flex flex-row text-2">Categores <ChevronDownIcon /></Text>
+  );
+};
+
+type Order = 'Most Members' | 'Most Active' | 'Most Recent';
+
+const ChooseOrderComboBox = ({
+  orders,
+}: {
+  orders: Order[];
+}) => {
+  return (
+    <Text className="flex flex-row text-2">Order <ChevronDownIcon /></Text>
+  );
+};
+
 export function ExploreSpaces({
   lang,
   spaces,
@@ -34,6 +56,17 @@ export function ExploreSpaces({
       ) : [],
     [spaces, categories],
   );
+  const tags = [
+    'Solar panels',
+    'Tokenomics',
+    'Regenerative economy',
+    'Web3',
+  ];
+  const orders: Order[] = [
+    'Most Members',
+    'Most Active',
+    'Most Recent',
+  ];
   const memberCount = 1342;
   const mintedTokens = '$1M';
   return (
@@ -44,30 +77,29 @@ export function ExploreSpaces({
       <div className="flex justify-center">
         <SpaceSearch />
       </div>
-      <div className="flex justify-center">
-        {/* Tags here */}
-        <></>
+      <div className="flex justify-center space-x-2 mt-2 mb-2">
+        {tags.map(tag => <span key={tag} className="text-1 text-neutral-500 border rounded-lg p-1">{tag}</span>)}
       </div>
       <Separator className="mt-1 mb-1" />
-      <div className="flex justify-around flex-row columns-3 space-x-3">
+      <div className="flex justify-around flex-row columns-3 space-x-3 mt-6 mb-6">
         <div className="flex flex-col">
-          <div className="flex justify-center">{spaces.length}</div>
-          <div className="flex justify-center">Spaces</div>
+          <div className="flex justify-center text-7 font-medium">{spaces.length}</div>
+          <div className="flex justify-center text-1 mt-2 text-neutral-500">Spaces</div>
           </div>
         <div className="flex flex-col">
-          <div className="flex justify-center">{memberCount}</div>
-          <div className="flex justify-center">Members</div>
+          <div className="flex justify-center text-7 font-medium">{memberCount}</div>
+          <div className="flex justify-center text-1 mt-2 text-neutral-500">Members</div>
         </div>
         <div className="flex flex-col">
-          <div className="flex justify-center">{mintedTokens}</div>
-          <div className="flex justify-center">Minted Tokens</div>
+          <div className="flex justify-center text-7 font-medium">{mintedTokens}</div>
+          <div className="flex justify-center text-1 mt-2 text-neutral-500">Minted Tokens</div>
         </div>
       </div>
       <Separator className="mt-1 mb-1" />
-      <div className="flex justify-left w-full">
+      <div className="flex flex-row w-full h-4 mt-10 mb-10">
         <Text className="text-5 text-left flex mb-8 grow">
           {categories ? (
-            <Text className="text-5 text-left flex mb-8">
+            <Text className="text-3 text-left flex mb-8">
               {categories.map((category, index) => (
                 <Text key={`cat-title-${index}`} className="ml-1 capitalize">
                   {index !== 0 && ' | '}
@@ -78,7 +110,7 @@ export function ExploreSpaces({
               {selectedSpaces?.length}
             </Text>
           ) : (
-            <Text className="text-5 text-left flex mb-8">
+            <Text className="text-3 text-left flex mb-8">
               <Text className="ml-1 capitalize">
                 All
               </Text>
@@ -87,10 +119,8 @@ export function ExploreSpaces({
             </Text>
           )}
         </Text>
-        {/* Choose Categores*/}
-        <Text className="flex flex-row">Categores <ChevronDownIcon /></Text>
-        {/* Choose Order*/}
-        <Text className="flex flex-row">Order <ChevronDownIcon /></Text>
+        <ChooseCategoriesComboBox categories={uniqueCategories} />
+        <ChooseOrderComboBox orders={orders} />
         <Link href={`/${lang}/network/create`} scroll={false}>
           <Button className="ml-2">
             <PlusIcon />
@@ -98,7 +128,7 @@ export function ExploreSpaces({
           </Button>
         </Link>
       </div>
-      <div className="space-y-6 flex mt-6">
+      <div className="space-y-6 flex mt-4">
         {categories ? (
           <SpaceCardList lang={lang} spaces={selectedSpaces} />
         ) : (
