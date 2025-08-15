aeb2a17709c0bf6d502b4364b9347911cbf02270
Sergey B. <sergey3bv@gmail.com>
2025-08-15 12:31:50 +0300

    fix: wrong field in webhook body schema

--- packages/core/src/common/server/webhooks/types.ts
+++ packages/core/src/common/server/webhooks/types.ts
@@ -5,7 +5,6 @@ export const schemaAlchemyWebhook = z.object({
   id: z.string(),
   createdAt: z.string().datetime(),
   type: z.union([z.literal('GRAPHQL'), z.string()]),
-  network: z.union([z.literal('BASE_MAINNET'), z.string()]),
   event: z.object({
     data: z.object({
       block: z.object({
@@ -14,6 +13,7 @@ export const schemaAlchemyWebhook = z.object({
       }),
     }),
     sequenceNumber: z.coerce.bigint(),
+    network: z.union([z.literal('BASE_MAINNET'), z.string()]),
   }),
 });
 
