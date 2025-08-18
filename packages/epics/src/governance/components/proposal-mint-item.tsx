532a0b51dd89f31eeaaea7c02fb2d2e13d6e1881
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-08-18 13:31:19 +0300

    feat(#1291): added loading state

--- packages/epics/src/governance/components/proposal-mint-item.tsx
+++ packages/epics/src/governance/components/proposal-mint-item.tsx
@@ -2,6 +2,7 @@
 
 import { usePersonByWeb3Address } from '../hooks';
 import { PersonAvatar } from '../../people/components/person-avatar';
+import { Skeleton } from '@hypha-platform/ui';
 
 interface ProposalMintItemProps {
   member: `0x${string}`;
@@ -10,18 +11,20 @@ interface ProposalMintItemProps {
 
 export const ProposalMintItem = ({ member, number }: ProposalMintItemProps) => {
   const originalNumber = Number(number) / Number(10n ** 18n);
-  const { person } = usePersonByWeb3Address(member);
+  const { person, isLoading } = usePersonByWeb3Address(member);
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-2">
-          <PersonAvatar avatarSrc={person.avatarUrl ?? ''} size="md" />
-          <div className="text-1 w-full">
-            {person.name} {person.surname}
+      <Skeleton loading={isLoading} className="h-7 w-full">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <PersonAvatar avatarSrc={person?.avatarUrl} size="md" />
+            <div className="text-1 w-full">
+              {person?.name} {person?.surname}
+            </div>
           </div>
+          <div className="text-1">{Number(originalNumber).toFixed(2)}</div>
         </div>
-        <div className="text-1">{Number(originalNumber).toFixed(2)}</div>
-      </div>
+      </Skeleton>
     </div>
   );
 };
