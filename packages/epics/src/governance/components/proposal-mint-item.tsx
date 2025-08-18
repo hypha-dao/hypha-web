a0ce299b94a2287bf0a4d65e4f73f1416233c8cf
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-08-18 12:56:45 +0300

    feat(#1291): implemented usePersonByWeb3Address hook to ProposalMintItem

--- packages/epics/src/governance/components/proposal-mint-item.tsx
+++ packages/epics/src/governance/components/proposal-mint-item.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { usePersonByWeb3Address } from '../hooks';
+import { PersonAvatar } from '../../people/components/person-avatar';
+
 interface ProposalMintItemProps {
   member: `0x${string}`;
   number: bigint;
@@ -7,10 +10,16 @@ interface ProposalMintItemProps {
 
 export const ProposalMintItem = ({ member, number }: ProposalMintItemProps) => {
   const originalNumber = Number(number) / Number(10n ** 18n);
+  const { person } = usePersonByWeb3Address(member);
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-3">
       <div className="flex justify-between items-center">
-        <div className="text-1 w-full">{member}</div>
+        <div className="flex items-center gap-2">
+          <PersonAvatar avatarSrc={person.avatarUrl ?? ''} size="md" />
+          <div className="text-1 w-full">
+            {person.name} {person.surname}
+          </div>
+        </div>
         <div className="text-1">{Number(originalNumber).toFixed(2)}</div>
       </div>
     </div>
