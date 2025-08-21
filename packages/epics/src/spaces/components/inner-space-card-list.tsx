72c1e938de7c5b6a9cb3464db7fa1fe43bc05708
Dmitry E <dsanichweb@gmail.com>
2025-08-21 14:26:15 +0300

    feat(#1330): added pagination for inner space list

---
+++ packages/epics/src/spaces/components/inner-space-card-list.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import {
+  Empty,
+  InnerSpaceCardContainer,
+  UseMembers,
+} from '@hypha-platform/epics';
+import { Locale } from '@hypha-platform/i18n';
+import { Space } from '@hypha-platform/core/server';
+import { useSpaceCardList } from '../hooks/use-space-card-list';
+import { SectionLoadMore } from '@hypha-platform/ui/server';
+import { Text } from '@radix-ui/themes';
+
+type InnerSpaceCardListProps = {
+  lang: Locale;
+  spaces: Space[];
+  pageSize?: number;
+  showLoadMore?: boolean;
+  useMembers: UseMembers;
+};
+
+export function InnerSpaceCardList({
+  lang,
+  spaces,
+  pageSize = 3,
+  showLoadMore = true,
+  useMembers,
+}: InnerSpaceCardListProps) {
+  const { pages, loadMore, pagination } = useSpaceCardList({
+    spaces,
+    pageSize,
+  });
+
+  return (
+    <>
+      {pagination?.totalPages > 0 ? (
+        <div className="flex flex-col justify-around items-center gap-4 mb-4">
+          <div className="w-full space-y-2">
+            {showLoadMore ? (
+              Array.from({ length: pages }).map((_, index) => {
+                const startIndex = index * pageSize;
+                const endIndex = startIndex + pageSize;
+                const pageSpaces = spaces.slice(startIndex, endIndex);
+                return (
+                  <InnerSpaceCardContainer
+                    key={index}
+                    spaces={pageSpaces}
+                    lang={lang}
+                    useMembers={useMembers}
+                    differentFirstCard={index === 0}
+                  />
+                );
+              })
+            ) : (
+              <InnerSpaceCardContainer
+                key={`spaces-${spaces.length}`}
+                spaces={spaces}
+                lang={lang}
+                useMembers={useMembers}
+                differentFirstCard={true}
+              />
+            )}
+          </div>
+          {showLoadMore && (
+            <SectionLoadMore
+              onClick={loadMore}
+              disabled={!pagination?.hasNextPage}
+            >
+              <Text>{pagination?.hasNextPage ? 'Load more' : 'No more'}</Text>
+            </SectionLoadMore>
+          )}
+        </div>
+      ) : (
+        <Empty>
+          <div className="flex flex-col gap-7">
+            <p>No spaces</p>
+          </div>
+        </Empty>
+      )}
+    </>
+  );
+}
---
+++ packages/epics/src/spaces/components/inner-space-card.container.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import {
+  getDhoPathGovernance,
+  InnerSpaceCardWrapper,
+  SpaceCard,
+  UseMembers,
+} from '@hypha-platform/epics';
+import Link from 'next/link';
+import { Locale } from '@hypha-platform/i18n';
+import { DEFAULT_SPACE_LEAD_IMAGE, Space } from '@hypha-platform/core/client';
+
+type SpaceCardContainerProps = {
+  lang: Locale;
+  spaces: Space[];
+  differentFirstCard?: boolean;
+  useMembers: UseMembers;
+};
+
+export const InnerSpaceCardContainer = ({
+  lang,
+  spaces,
+  differentFirstCard = false,
+  useMembers,
+}: SpaceCardContainerProps) => (
+  <div
+    data-testid="member-spaces-container"
+    className="grid grid-cols-1 sm:grid-cols-3 gap-2"
+  >
+    {spaces.map((space, index) =>
+      space.slug ? (
+        <div key={space.id}>
+          <Link
+            href={getDhoPathGovernance(lang, space.slug)}
+            aria-label={`View governance for ${space.title}`}
+          >
+            {index === 0 && differentFirstCard ? (
+              <SpaceCard
+                description={space.description || ''}
+                icon={space.logoUrl || ''}
+                leadImage={space.leadImage || DEFAULT_SPACE_LEAD_IMAGE}
+                members={space.memberCount}
+                agreements={space.documentCount}
+                title={space.title || ''}
+              />
+            ) : (
+              <InnerSpaceCardWrapper
+                spaceSlug={space.slug}
+                title={space.title}
+                description={space.description as string}
+                leadImageUrl={space.leadImage || DEFAULT_SPACE_LEAD_IMAGE}
+                useMembers={useMembers}
+              />
+            )}
+          </Link>
+        </div>
+      ) : null,
+    )}
+  </div>
+);
