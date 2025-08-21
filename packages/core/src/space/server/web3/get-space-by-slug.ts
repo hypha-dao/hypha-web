eb4a5b059d44bfca09acc267079568023a1e638f
Dmitry E <dsanichweb@gmail.com>
2025-08-21 13:09:24 +0300

    feat(#1330): changed card for first items in space list

---
+++ packages/core/src/space/server/web3/get-space-by-slug.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { db } from '@hypha-platform/storage-postgres';
+import { findSpaceBySlug } from '@hypha-platform/core/server';
+import { Space } from '@hypha-platform/core/client';
+import {
+  fetchSpaceDetails,
+  fetchSpaceProposalsIds,
+} from '@hypha-platform/core/client';
+
+interface GetSpaceBySlugProps {
+  slug: string;
+}
+
+export async function getSpaceBySlug(
+  { slug }: GetSpaceBySlugProps,
+): Promise<Space | null> {
+  try {
+    const space = await findSpaceBySlug({ slug }, { db });
+
+    if (!space) return null;
+
+    const spacesWithWeb3Id = BigInt(space?.web3SpaceId ?? 0);
+    if (spacesWithWeb3Id === 0n) {
+      return space;
+    }
+
+    const web3SpaceIds = [spacesWithWeb3Id];
+    const [web3details, web3proposalsIds] = await Promise.all([
+      fetchSpaceDetails({ spaceIds: web3SpaceIds }),
+      fetchSpaceProposalsIds({ spaceIds: web3SpaceIds }),
+    ]);
+
+    const [spaceDetails] = web3details;
+    const [spaceProposals] = web3proposalsIds;
+  
+    return {
+      ...space,
+      memberCount: spaceDetails?.members?.length ?? 0,
+      memberAddresses: Array.isArray(spaceDetails?.members)
+        ? (spaceDetails!.members
+            .filter((m): m is string => typeof m === 'string')
+            .map((m) => m.toLowerCase()) as `0x{string}`[])
+        : [],
+      documentCount: spaceProposals?.accepted.length ?? 0,
+    };
+  } catch (error) {
+    throw new Error('Failed to get space', {
+      cause: error instanceof Error ? error : new Error(String(error)),
+    });
+  }
+}
--- packages/epics/src/spaces/components/subspace-section.tsx
+++ packages/epics/src/spaces/components/subspace-section.tsx
@@ -1,7 +1,11 @@
 import { Text } from '@radix-ui/themes';
 import Link from 'next/link';
 import { PlusIcon } from '@radix-ui/react-icons';
-import { UseMembers, InnerSpaceCardWrapper } from '@hypha-platform/epics';
+import {
+  UseMembers,
+  InnerSpaceCardWrapper,
+  SpaceCard,
+} from '@hypha-platform/epics';
 import { DEFAULT_SPACE_LEAD_IMAGE, Space } from '@hypha-platform/core/client';
 import { Locale } from '@hypha-platform/i18n';
 import { AuthenticatedLinkButton } from '../../../../../apps/web/src/app/[lang]/dho/[id]/_components/authenticated-link-button';
@@ -33,23 +37,37 @@ export const SubspaceSection = ({
       {!spaces.length ? (
         <span className="text-2 text-center text-neutral-11">
           {' '}
-          No inner spaces
+          No spaces
         </span>
       ) : (
         <div
           data-testid="sub-spaces-container"
           className="grid grid-cols-1 sm:grid-cols-3 gap-3"
         >
-          {spaces.map((space) => (
+          {spaces.map((space, index) => (
             <div key={space.id} className="mb-1">
-              <Link href={getSpaceDetailLink(lang, space.slug as string)}>
-                <InnerSpaceCardWrapper
-                  spaceSlug={space.slug}
-                  title={space.title}
-                  description={space.description as string}
-                  leadImageUrl={space.leadImage || DEFAULT_SPACE_LEAD_IMAGE}
-                  useMembers={useMembers}
-                />
+              <Link
+                href={getSpaceDetailLink(lang, space.slug as string)}
+                aria-label={`View governance for ${space.title}`}
+              >
+                {index === 0 ? (
+                  <SpaceCard
+                    description={space.description || ''}
+                    icon={space.logoUrl || ''}
+                    leadImage={space.leadImage || DEFAULT_SPACE_LEAD_IMAGE}
+                    members={space.memberCount}
+                    agreements={space.documentCount}
+                    title={space.title || ''}
+                  />
+                ) : (
+                  <InnerSpaceCardWrapper
+                    spaceSlug={space.slug}
+                    title={space.title}
+                    description={space.description as string}
+                    leadImageUrl={space.leadImage || DEFAULT_SPACE_LEAD_IMAGE}
+                    useMembers={useMembers}
+                  />
+                )}
               </Link>
             </div>
           ))}
