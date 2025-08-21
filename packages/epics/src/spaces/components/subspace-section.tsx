dadb7bb1746dc6e4aa7809a5e95a8539ca05728e
Dmitry E <dsanichweb@gmail.com>
2025-08-21 15:25:05 +0300

    feat(#1330): clean up

--- packages/epics/src/spaces/components/subspace-section.tsx
+++ packages/epics/src/spaces/components/subspace-section.tsx
@@ -1,18 +1,13 @@
 import { Text } from '@radix-ui/themes';
 import Link from 'next/link';
 import { PlusIcon } from '@radix-ui/react-icons';
-import {
-  UseMembers,
-  InnerSpaceCardWrapper,
-  SpaceCard,
-} from '@hypha-platform/epics';
-import { DEFAULT_SPACE_LEAD_IMAGE, Space } from '@hypha-platform/core/client';
+import { UseMembers } from '@hypha-platform/epics';
+import { Space } from '@hypha-platform/core/client';
 import { Locale } from '@hypha-platform/i18n';
 import { AuthenticatedLinkButton } from '../../../../../apps/web/src/app/[lang]/dho/[id]/_components/authenticated-link-button';
 import { InnerSpaceCardList } from './inner-space-card-list';
 
 interface SubspaceSectionProps {
-  getSpaceDetailLink: (lang: Locale, id: string) => string;
   spaces: Space[];
   lang: Locale;
   useMembers: UseMembers;
@@ -21,7 +16,6 @@ interface SubspaceSectionProps {
 export const SubspaceSection = ({
   spaces,
   lang,
-  getSpaceDetailLink,
   useMembers,
 }: SubspaceSectionProps) => {
   return (
