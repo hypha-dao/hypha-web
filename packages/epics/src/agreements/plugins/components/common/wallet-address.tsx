20aa2d1476890f9a4efaa7cde87373f0b6cc3b8e
Dmitry E <dsanichweb@gmail.com>
2025-08-26 12:02:51 +0300

    feat(#1417): added asterisk wallet address

--- packages/epics/src/agreements/plugins/components/common/wallet-address.tsx
+++ packages/epics/src/agreements/plugins/components/common/wallet-address.tsx
@@ -1,4 +1,5 @@
 import { Input } from '@hypha-platform/ui';
+import { AsteriskIcon } from 'lucide-react';
 
 interface WalletAddressProps {
   address: string;
@@ -12,7 +13,13 @@ export const WalletAddress = ({ address, onChange }: WalletAddressProps) => {
 
   return (
     <div className="flex flex-col md:flex-row w-full md:items-center md:justify-between gap-4">
-      <label className="text-2 text-neutral-11">Wallet Address</label>
+      <label className="text-2 text-neutral-11 flex flex-row">
+        Wallet Address
+        <AsteriskIcon
+          size={12}
+          className="text-destructive w-4 h-4 left-0 align-super"
+        />
+      </label>
       <div>
         <Input
           value={address}
