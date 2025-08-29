423cf4dea940a35c260fb2eb3f5c6a397f68b46c
Dmitry E <dsanichweb@gmail.com>
2025-08-29 17:34:32 +0300

    feat(#1418): added woraround to force update form data on final load

--- apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
@@ -58,32 +58,45 @@ export default function SpaceConfiguration() {
         }
         className="-m-4 lg:-m-7"
       >
-        <SpaceForm
-          submitLabel="Update"
-          submitLoadingLabel="Updating..."
-          isLoading={isLoadingJwt || isLoading || isMutating}
-          closeUrl={closeUrl}
-          backUrl={`${closeUrl}${PATH_SELECT_SETTINGS_ACTION}`}
-          backLabel="Back to Settings"
-          creator={{
-            name: person?.name,
-            surname: person?.surname,
-          }}
-          onSubmit={updateSpace}
-          defaultValues={{
-            ...space,
-            title: space?.title || '',
-            description: space?.description || '',
-            slug: space?.slug || '',
-            logoUrl: space?.logoUrl || '',
-            leadImage: space?.leadImage || '',
-            categories: space?.categories || [],
-            links: space?.links || [],
-            web3SpaceId: space?.web3SpaceId || undefined,
-            parentId: space?.parentId || null,
-            address: space?.address || '',
-          }}
-        />
+        {isLoadingJwt || isLoading || isMutating ? (
+          <SpaceForm
+            submitLabel="Update"
+            submitLoadingLabel="Updating..."
+            isLoading={true}
+            closeUrl={closeUrl}
+            backUrl={`${closeUrl}${PATH_SELECT_SETTINGS_ACTION}`}
+            backLabel="Back to Settings"
+            creator={{}}
+            onSubmit={updateSpace}
+          />
+        ) : (
+          <SpaceForm
+            submitLabel="Update"
+            submitLoadingLabel="Updating..."
+            isLoading={false}
+            closeUrl={closeUrl}
+            backUrl={`${closeUrl}${PATH_SELECT_SETTINGS_ACTION}`}
+            backLabel="Back to Settings"
+            creator={{
+              name: person?.name,
+              surname: person?.surname,
+            }}
+            onSubmit={updateSpace}
+            defaultValues={{
+              ...space,
+              title: space?.title || '',
+              description: space?.description || '',
+              slug: space?.slug || '',
+              logoUrl: space?.logoUrl || '',
+              leadImage: space?.leadImage || '',
+              categories: space?.categories || [],
+              links: space?.links || [],
+              web3SpaceId: space?.web3SpaceId || undefined,
+              parentId: space?.parentId || null,
+              address: space?.address || '',
+            }}
+          />
+        )}
       </LoadingBackdrop>
     </SidePanel>
   );
