8f6442f44e31fee3b0590e027daf6f227e4b2fbc
Dmitry E <dsanichweb@gmail.com>
2025-09-03 12:32:08 +0300

    feat(#1420): refactored

--- packages/storage-postgres/src/schema/flags.ts
+++ packages/storage-postgres/src/schema/flags.ts
@@ -1,4 +1,4 @@
 import { pgEnum } from 'drizzle-orm/pg-core';
 import { SPACE_FLAGS } from '../../../core/src/categories';
 
-export const spaceFlags = pgEnum('flags', SPACE_FLAGS);
+export const spaceFlags = pgEnum('space_flags', SPACE_FLAGS);
