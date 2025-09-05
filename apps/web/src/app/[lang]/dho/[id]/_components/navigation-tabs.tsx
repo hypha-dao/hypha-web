567ae92c63da3d11412573237795782a01f903ad
Dmitry E <dsanichweb@gmail.com>
2025-09-05 18:14:50 +0300

    TEMP

--- apps/web/src/app/[lang]/dho/[id]/_components/navigation-tabs.tsx
+++ apps/web/src/app/[lang]/dho/[id]/_components/navigation-tabs.tsx
@@ -9,14 +9,69 @@ import { getDhoPathMembership } from '../@tab/membership/constants';
 import { getActiveTabFromPath } from './get-active-tab-from-path';
 import { getDhoPathTreasury } from '../@tab/treasury/constants';
 import { getDhoPathOrganisation } from '../@tab/organisation/constants';
+import useEmblaCarousel from 'embla-carousel-react';
 
 export function NavigationTabs({ lang, id }: { lang: Locale; id: string }) {
   const pathname = usePathname();
+  const [emblaRef] = useEmblaCarousel({
+    align: 'start',
+    axis: 'x',
+    startIndex: 0,
+  });
   const activeTab = getActiveTabFromPath(pathname);
 
+  const tabs = [
+    {
+      title: 'Organisation',
+      name: 'organisation',
+      href: getDhoPathOrganisation(lang, id),
+    },
+    {
+      title: 'Governance',
+      name: 'governance',
+      href: getDhoPathGovernance(lang, id),
+    },
+    {
+      title: 'Membership',
+      name: 'membership',
+      href: getDhoPathMembership(lang as Locale, id as string),
+    },
+    {
+      title: 'Treasury',
+      name: 'treasury',
+      href: getDhoPathTreasury(lang as Locale, id as string),
+    },
+  ];
+
   return (
-    <Tabs value={activeTab} className="w-full mt-16">
-      <TabsList className="w-full mb-4">
+    <Tabs
+      value={activeTab}
+      className="w-full mt-16 overflow-hidden"
+      ref={emblaRef}
+    >
+      {/* <div className="overflow-hidden" ref={emblaRef}> */}
+      {/* <TabsList className="flex touch-pan-y w-full mb-4"> */}
+      <TabsList className="flex mb-4">
+        {tabs.map((tab) => (
+          <TabsTrigger
+            asChild
+            key={tab.name}
+            value={tab.name}
+            // className="w-full"
+            className="flex-none"
+            variant="ghost"
+            role="group"
+            aria-roledescription="slide"
+            // style="flex: 0 0 auto;"
+          >
+            <Link href={tab.href} className="w-full" passHref>
+              {tab.title}
+            </Link>
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {/* </div> */}
+      {/* <TabsList className="w-full mb-4">
         <TabsTrigger
           asChild
           value="organisation"
@@ -73,7 +128,7 @@ export function NavigationTabs({ lang, id }: { lang: Locale; id: string }) {
             Treasury
           </Link>
         </TabsTrigger>
-      </TabsList>
+      </TabsList> */}
     </Tabs>
   );
 }
