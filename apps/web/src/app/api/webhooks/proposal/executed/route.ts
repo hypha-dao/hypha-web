87e789100e6d6533f24fedc9a0eb37f0ff959cbd
Sergey B. <sergey3bv@gmail.com>
2025-09-09 14:12:19 +0300

    feat: strict check for webhook signing key

--- apps/web/src/app/api/webhooks/proposal/executed/route.ts
+++ apps/web/src/app/api/webhooks/proposal/executed/route.ts
@@ -3,7 +3,12 @@ import { daoProposalsImplementationAbi } from '@hypha-platform/core/generated';
 
 export const POST = Alchemy.newHandler(
   {
-    signingKey: process.env.WH_PROPOSAL_EXECUTED_SIGN_KEY ?? '',
+    signingKey: (() => {
+      const key = process.env.WH_PROPOSAL_EXECUTED_SIGN_KEY;
+      if (!key) throw new Error('Missing key for proposal executed webhook');
+
+      return key;
+    })(),
     abi: daoProposalsImplementationAbi,
     event: 'ProposalExecuted',
   },
--- apps/web/src/app/api/webhooks/proposal/rejected/route.ts
+++ apps/web/src/app/api/webhooks/proposal/rejected/route.ts
@@ -3,7 +3,12 @@ import { daoProposalsImplementationAbi } from '@hypha-platform/core/generated';
 
 export const POST = Alchemy.newHandler(
   {
-    signingKey: process.env.WH_PROPOSAL_REJECTED_SIGN_KEY ?? '',
+    signingKey: (() => {
+      const key = process.env.WH_PROPOSAL_REJECTED_SIGN_KEY;
+      if (!key) throw new Error('Missing key for proposal rejected webhook');
+
+      return key;
+    })(),
     abi: daoProposalsImplementationAbi,
     event: 'ProposalRejected',
   },
