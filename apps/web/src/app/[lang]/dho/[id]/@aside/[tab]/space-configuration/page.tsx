ae4c3f8d991a338ad36b5f64c734c63e4e1445c1
Dmitry E <dsanichweb@gmail.com>
2025-09-11 11:17:03 +0300

    feat(#1400): changed redirect to overview tab containting updated space

--- apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
@@ -12,9 +12,9 @@ import React from 'react';
 import { LoadingBackdrop } from '@hypha-platform/ui/server';
 import { Button } from '@hypha-platform/ui';
 import { useRouter } from 'next/navigation';
-import { getDhoPathAgreements } from '../../../@tab/agreements/constants';
 import { Locale } from '@hypha-platform/i18n';
 import { PATH_SELECT_SETTINGS_ACTION } from '@web/app/constants';
+import { getDhoPathOverview } from '../../../@tab/overview/constants';
 
 export default function SpaceConfiguration() {
   const { person } = useMe();
@@ -27,7 +27,7 @@ export default function SpaceConfiguration() {
 
   React.useEffect(() => {
     if (progress === 100 && !isPending && spaceSlug) {
-      router.push(getDhoPathAgreements(lang as Locale, spaceSlug));
+      router.push(getDhoPathOverview(lang as Locale, spaceSlug));
     }
   }, [progress, isPending, spaceSlug, lang]);
 
