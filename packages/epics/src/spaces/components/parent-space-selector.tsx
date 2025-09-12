cfb55bd0c29b916271788777380fe8c70328c29e
Dmitry E <dsanichweb@gmail.com>
2025-09-12 15:55:35 +0300

    feat(#1400): updated parent selector width

--- packages/epics/src/spaces/components/parent-space-selector.tsx
+++ packages/epics/src/spaces/components/parent-space-selector.tsx
@@ -44,14 +44,16 @@ export const ParentSpaceSelector = ({
         className={cn('flex flex-row gap-1', className)}
         aria-disabled={isLoading}
       >
-        <div className="pt-1 pb-1 align-middle">
-          <Label className="text-sm text-neutral-11">Linked to</Label>
-        </div>
-        <div className="flex flex-col grow-0">
+        <span className="flex flex-row">
+          <div className="pt-1 pb-1 align-middle">
+            <Label className="text-sm text-neutral-11">Linked to</Label>
+          </div>
+        </span>
+        <span className="flex grow"></span>
+        <span className="flex flex-row gap-1">
           <Combobox
             options={options ?? []}
             initialValue={parentSpaceIdString}
-            className="border-0 md:w-40"
             disabled={checked || isLoading}
             onChange={(value: string) => {
               const parentId = Number.parseInt(value);
@@ -61,22 +63,21 @@ export const ParentSpaceSelector = ({
             }}
             allowEmptyChoice={checked}
           />
-        </div>
-        <div className="flex grow"></div>
-        <div className="h-4 w-4">
-          <Input
-            id={checkboxId}
-            ref={checkboxRef}
-            type="checkbox"
-            checked={checked}
-            onChange={(e) => setChecked(e.target.checked)}
-          />
-        </div>
-        <div className="pt-1 pb-1 align-middle">
-          <Label htmlFor={checkboxId} className="text-sm text-neutral-11">
-            Root Space
-          </Label>
-        </div>
+          <div className="h-4 w-4">
+            <Input
+              id={checkboxId}
+              ref={checkboxRef}
+              type="checkbox"
+              checked={checked}
+              onChange={(e) => setChecked(e.target.checked)}
+            />
+          </div>
+          <div className="pt-1 pb-1 align-middle">
+            <Label htmlFor={checkboxId} className="text-sm text-neutral-11">
+              Root Space
+            </Label>
+          </div>
+        </span>
       </div>
       {checked && (
         <span className="text-1 text-neutral-11">
