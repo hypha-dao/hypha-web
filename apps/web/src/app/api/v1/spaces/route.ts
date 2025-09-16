bd2a3f64865eff14ecc0b928150add88d429bd2f
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-09-16 13:04:47 +0300

    feat(#683): updated query params in api/v1/spaces route

--- apps/web/src/app/api/v1/spaces/route.ts
+++ apps/web/src/app/api/v1/spaces/route.ts
@@ -15,16 +15,16 @@ export async function GET(request: NextRequest) {
 
     const parentOnlyParam = queryParams.get('parentOnly');
     const parentOnly =
-      parentOnlyParam === 'true'
-        ? true
-        : parentOnlyParam === 'false'
-        ? false
-        : undefined;
-    const options = parentOnly !== undefined ? { parentOnly } : {};
+      parentOnlyParam !== null ? parentOnlyParam === 'true' : undefined;
 
-    let spaces = web3SpaceIds
-      ? await getSpacesByWeb3Ids(web3SpaceIds, options)
-      : await getAllSpaces();
+    let spaces;
+
+    if (web3SpaceIds) {
+      const options = parentOnly !== undefined ? { parentOnly } : undefined;
+      spaces = await getSpacesByWeb3Ids(web3SpaceIds, options);
+    } else {
+      spaces = await getAllSpaces();
+    }
 
     return NextResponse.json(spaces);
   } catch (error) {
