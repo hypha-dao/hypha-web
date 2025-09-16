6a708218fe0bbd9fd3eebedd67082384cfc443e4
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-09-16 14:09:20 +0300

    feat(#683): formatting

--- packages/epics/src/governance/hooks/use-spaces-by-web3-ids-client.ts
+++ packages/epics/src/governance/hooks/use-spaces-by-web3-ids-client.ts
@@ -28,7 +28,7 @@ export const useSpacesByWeb3Ids = (
   const { data: spaces, isLoading } = useSWR(endpoint, (endpoint) =>
     fetch(endpoint, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
       },
     }).then((res) => res.json()),
   );
