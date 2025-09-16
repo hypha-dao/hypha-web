e48a6a881e4b9ab9c0499793a64dd91b4468b719
Dmitry E <dsanichweb@gmail.com>
2025-09-16 17:58:46 +0300

    feat(#1387): added redirect to hypha.earth on prod from index

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 //@ts-check
 
 const IMAGE_HOSTS = process.env.NEXT_PUBLIC_IMAGE_HOSTS?.split(', ') ?? [];
+const ROOT_URL = process.env.NEXT_PUBLIC_ROOT_URL ?? 'https://hypha.earth';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -32,6 +33,19 @@ const nextConfig = {
       hostname,
     })),
   },
+  redirects: async () => {
+    if (process.env.NODE_ENV === 'development') {
+      return [];
+    } else {
+      return [
+        {
+          source: '/',
+          destination: ROOT_URL,
+          permanent: true,
+        },
+      ];
+    }
+  }
 };
 
 export default nextConfig;
