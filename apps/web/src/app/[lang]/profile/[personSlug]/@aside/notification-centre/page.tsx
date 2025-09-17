0d36143a47d2185596b826f9029d70bc276d634b
Dmitry E <dsanichweb@gmail.com>
2025-09-17 12:12:05 +0300

    feat(#1124): added dummy notification centre form

--- apps/web/src/app/[lang]/profile/[personSlug]/@aside/notification-centre/page.tsx
+++ apps/web/src/app/[lang]/profile/[personSlug]/@aside/notification-centre/page.tsx
@@ -1,5 +1,33 @@
 'use client';
 
+import { NotificationCentreForm, SidePanel } from '@hypha-platform/epics';
+import { Button, LoadingBackdrop } from '@hypha-platform/ui';
+
 export default function NotificationCentre() {
-  return <></>;
+  const progress = 0;
+  const isBusy = false;
+  const isError = undefined;
+  const reset = () => {};
+  const currentAction = undefined;
+  return (
+    <SidePanel>
+      <LoadingBackdrop
+        progress={progress}
+        isLoading={isBusy}
+        message={
+          isError ? (
+            <div className="flex flex-col">
+              <div>Ouh Snap. There was an error</div>
+              <Button onClick={reset}>Reset</Button>
+            </div>
+          ) : (
+            <div>{currentAction}</div>
+          )
+        }
+        className="-m-4 lg:-m-7"
+      >
+        <NotificationCentreForm />
+      </LoadingBackdrop>
+    </SidePanel>
+  );
 }
