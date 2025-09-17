78215e5361700196b52f3c16fb644c3661763194
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-09-17 13:58:43 +0300

    feat(#683): added component

---
+++ packages/epics/src/governance/components/proposal-activate-spaces-data.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+interface ProposalActivateSpacesDataProps {}
+
+const HYPHA_PRICE_USD = 0.25;
+const HYPHA_PER_MONTH = 44;
+const USDC_PER_MONTH = 11;
+
+export const ProposalActivateSpacesData =
+  ({}: ProposalActivateSpacesDataProps) => {
+    return <></>;
+  };
