c18a0f7dcb2750b461e2d849cacb8fc6eecd0d9d
Sergey B. <sergey3bv@gmail.com>
2025-09-18 09:58:18 +0000

    refactor: moving constants to props

--- packages/epics/src/treasury/hooks/use-user-assets-section.ts
+++ packages/epics/src/treasury/hooks/use-user-assets-section.ts
@@ -5,13 +5,22 @@ import { formatCurrencyValue } from '@hypha-platform/ui-utils';
 import { FILTER_OPTIONS_ASSETS } from '../../common/constants';
 import { useUserAssets } from './use-user-assets';
 
+export interface UseUserAssetsSectionProps {
+  personSlug?: string;
+  pageSize?: number;
+  filterOptions?: {
+    label: string;
+    value: string;
+  }[];
+}
+
 export const useUserAssetsSection = ({
   personSlug,
-}: {
-  personSlug?: string;
-}) => {
+  pageSize = 3,
+  filterOptions = FILTER_OPTIONS_ASSETS,
+}: UseUserAssetsSectionProps = {}) => {
   const [activeFilter, setActiveFilter] = React.useState('all');
-  const [visibleCount, setVisibleCount] = React.useState(3);
+  const [visibleCount, setVisibleCount] = React.useState(pageSize);
 
   const { isLoading, balance, assets } = useUserAssets({
     ...(activeFilter !== 'all' && { filter: { type: activeFilter } }),
@@ -19,12 +28,12 @@ export const useUserAssetsSection = ({
   });
 
   React.useEffect(() => {
-    setVisibleCount(3);
-  }, [activeFilter, assets]);
+    setVisibleCount(pageSize);
+  }, [activeFilter, assets.length, pageSize]);
 
   const loadMore = React.useCallback(() => {
-    setVisibleCount((prev) => prev + 3);
-  }, []);
+    setVisibleCount((prev) => prev + pageSize);
+  }, [pageSize]);
 
   const totalBalance = `$ ${formatCurrencyValue(balance)}`;
   const visibleAssets = assets.slice(0, visibleCount);
@@ -37,7 +46,7 @@ export const useUserAssetsSection = ({
     hasMore,
     activeFilter,
     setActiveFilter,
-    filterOptions: FILTER_OPTIONS_ASSETS,
+    filterOptions,
     totalBalance,
   };
 };
