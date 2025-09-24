23223992de8db7fffa0ea416314c03445d16466f
Sergey B. <sergey3bv@gmail.com>
2025-09-24 20:28:12 +0300

    feat: proposal creation notification for space members

--- apps/web/src/app/api/webhooks/proposal/created/route.ts
+++ apps/web/src/app/api/webhooks/proposal/created/route.ts
@@ -2,7 +2,9 @@ import {
   Alchemy,
   findPersonByWeb3Address,
   findSpaceByWeb3Id,
+  findPeopleByWeb3Addresses,
 } from '@hypha-platform/core/server';
+import { fetchSpaceDetails } from '@hypha-platform/core/client';
 import { daoProposalsImplementationAbi } from '@hypha-platform/core/generated';
 import { db } from '@hypha-platform/storage-postgres';
 import {
@@ -11,7 +13,9 @@ import {
 } from '@hypha-platform/notifications/server';
 import {
   emailProposalCreationForCreator,
+  emailProposalCreationForMembers,
   pushProposalCreationForCreator,
+  pushProposalCreationForMembers,
 } from '@hypha-platform/notifications/template';
 
 export const POST = Alchemy.newHandler(
@@ -98,6 +102,84 @@ export const POST = Alchemy.newHandler(
       .forEach(({ reason }) =>
         console.error(
           'Failed to notify creators about proposal creation:',
+          reason,
+        ),
+      );
+  },
+  async (events) => {
+    const spaceIds = events.map(({ args }) => args.spaceId);
+
+    const spacesDetails = await (async () => {
+      try {
+        return await fetchSpaceDetails({ spaceIds });
+      } catch (e) {
+        console.error(
+          'Failed to fetch space details for proposal creation:',
+          e,
+        );
+      }
+    })();
+    if (!spacesDetails || spacesDetails.length === 0) {
+      console.warn(
+        'Zero spaces found in the blockchain for the "ProposalCreation" event.',
+        'Proposal IDs:',
+        events.map(({ args }) => args.proposalId),
+      );
+
+      return;
+    }
+
+    const fetchingData = spacesDetails.map(async ({ members, spaceId }) => {
+      const people = await findPeopleByWeb3Addresses(
+        {
+          addresses: members as string[],
+        },
+        { db },
+      );
+      const space = await findSpaceByWeb3Id({ id: Number(spaceId) }, { db });
+
+      return { people, space };
+    });
+    const spacesWithPeople = (await Promise.allSettled(fetchingData))
+      .filter((res) => res.status === 'fulfilled')
+      .map(({ value }) => value)
+      .filter((space) => space.people.length > 0);
+
+    const notificationParams = spacesWithPeople.map(({ space, people }) => ({
+      slugs: people.map(({ slug }) => slug!),
+      spaceTitle: space?.title,
+      spaceSlug: space?.slug,
+    }));
+
+    const sendingEmails = notificationParams.map(async (params) => {
+      const { body, subject } = emailProposalCreationForMembers(params);
+
+      return await sendEmailByAlias({
+        app_id: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID ?? '',
+        alias: {
+          include_aliases: { external_id: params.slugs },
+        },
+        content: { email_body: body, email_subject: subject },
+      });
+    });
+    const sendingPushes = notificationParams.map(async (params) => {
+      const { contents, headings } = pushProposalCreationForMembers(params);
+
+      return await sendPushByAlias({
+        app_id: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID ?? '',
+        alias: {
+          include_aliases: { external_id: params.slugs },
+        },
+        content: { contents, headings },
+      });
+    });
+
+    const notifying = Promise.allSettled(sendingEmails.concat(sendingPushes));
+    (await notifying)
+      .filter((res) => res.status === 'rejected')
+      .forEach(({ reason }) =>
+        console.error(
+          'Failed to notify space members about proposal creation:',
           reason,
         ),
       );
