7a96330abbac02feea02e43ea1ec579aeb45e66d
Dmitry E <dsanichweb@gmail.com>
2025-09-30 17:41:55 +0300

    feat(#1124): updated constants

--- packages/notifications/src/hooks/use-notificatons.ts
+++ packages/notifications/src/hooks/use-notificatons.ts
@@ -3,28 +3,20 @@
 import { useMe } from '@hypha-platform/core/client';
 import React from 'react';
 import OneSignal from 'react-onesignal';
+import {
+  OptionTag,
+  Tag,
+  TAG_EMAIL,
+  TAG_NEW_PROPOSAL_OPEN,
+  TAG_PROPOSAL_APPROVED_OR_REJECTED,
+  TAG_PUSH,
+  TAG_SUBSCRIBED,
+} from '../constants';
 
 const DEV_ENV = process.env.NODE_ENV === 'development';
 
 const TRUE = 'true';
 const FALSE = 'false';
-
-export const TAG_SUBSCRIBED = 'subscribed';
-export const TAG_PUSH = 'push';
-export const TAG_EMAIL = 'email';
-export const TAG_NEW_PROPOSAL_OPEN = 'opt_newProposalOpen';
-export const TAG_PROPOSAL_APPROVED_OR_REJECTED =
-  'opt_proposalApprovedOrRejected';
-
-export const MAIN_TAGS = [TAG_SUBSCRIBED, TAG_PUSH, TAG_EMAIL] as const;
-export const OPTION_TAGS = [
-  TAG_NEW_PROPOSAL_OPEN,
-  TAG_PROPOSAL_APPROVED_OR_REJECTED,
-] as const;
-export const TAGS = [...MAIN_TAGS, ...OPTION_TAGS] as const;
-export type Tag = (typeof TAGS)[number];
-export type MainTag = (typeof MAIN_TAGS)[number];
-export type OptionTag = (typeof OPTION_TAGS)[number];
 
 export interface INotificationsContext {
   initialized: boolean;
