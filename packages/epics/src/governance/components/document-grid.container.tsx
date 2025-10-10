525a17c1e3f2aba5252ef9c90fa35d2f9450cafc
Dima | Dmitry Ermolenko <dsanichweb@gmail.com>
2025-10-10 17:00:19 +0300

    feat(#1613): refactored
    Co-authored-by: Sergey B. <sergey3bv@gmail.com>

--- packages/epics/src/governance/components/document-grid.container.tsx
+++ packages/epics/src/governance/components/document-grid.container.tsx
@@ -21,7 +21,7 @@ export const DocumentGridContainer = ({
 }: DocumentGridContainerProps) => {
   const { page, firstPageSize, pageSize } = pagination;
   const startIndex =
-    page < 1 ? 0 : page === 1 ? 0 : firstPageSize + (page - 2) * pageSize;
+    page =< 1 ? 0 : firstPageSize + (page - 2) * pageSize;
   const endIndex = Math.min(
     documents.length,
     page < 1 ? 0 : page === 1 ? firstPageSize : startIndex + pageSize,
