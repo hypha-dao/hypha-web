ed9c487fc60fae06421b7d66d8c6d31faa2d7d44
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-10-17 11:56:31 +0300

    feat(#1584): added votes label

--- packages/epics/src/governance/components/voter-list.tsx
+++ packages/epics/src/governance/components/voter-list.tsx
@@ -8,6 +8,7 @@ import {
   TooltipContent,
   TooltipTrigger,
   Button,
+  Label,
 } from '@hypha-platform/ui';
 import Link from 'next/link';
 
@@ -17,28 +18,31 @@ export const VoterList = ({ documentSlug }: { documentSlug: string }) => {
   const pathname = usePathname();
 
   return voters && voters.length > 0 ? (
-    <div className="flex gap-2 overflow-x-auto items-center">
-      {voters.slice(0, maxVisibleVoters).map((voter) => (
-        <Tooltip key={voter.name}>
-          <TooltipContent>
-            {voter.name} voted {voter.vote}
-          </TooltipContent>
-          <TooltipTrigger>
-            <PersonAvatar
-              key={voter.name}
-              avatarSrc={voter.avatarUrl}
-              userName={voter.name}
-            />
-          </TooltipTrigger>
-        </Tooltip>
-      ))}
-      {voters.length > maxVisibleVoters && (
-        <Link href={`${pathname}/voters`} scroll={false}>
-          <Button variant="ghost" className="text-accent-11">
-            and {voters.length - maxVisibleVoters} more
-          </Button>
-        </Link>
-      )}
+    <div className="flex flex-col gap-6">
+      <Label>Votes</Label>
+      <div className="flex gap-2 overflow-x-auto items-center">
+        {voters.slice(0, maxVisibleVoters).map((voter) => (
+          <Tooltip key={voter.name}>
+            <TooltipContent>
+              {voter.name} voted {voter.vote}
+            </TooltipContent>
+            <TooltipTrigger>
+              <PersonAvatar
+                key={voter.name}
+                avatarSrc={voter.avatarUrl}
+                userName={voter.name}
+              />
+            </TooltipTrigger>
+          </Tooltip>
+        ))}
+        {voters.length > maxVisibleVoters && (
+          <Link href={`${pathname}/voters`} scroll={false}>
+            <Button variant="ghost" className="text-accent-11">
+              and {voters.length - maxVisibleVoters} more
+            </Button>
+          </Link>
+        )}
+      </div>
     </div>
   ) : null;
 };
