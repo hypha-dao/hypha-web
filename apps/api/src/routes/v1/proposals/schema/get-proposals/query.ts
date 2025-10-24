c688ec0abda76bd7e0b92f20f37b695a4c85ec9e
Sergey B. <sergey3bv@gmail.com>
2025-10-24 14:27:18 +0300

    feat(api): schema was updated to match openapi specification

--- apps/api/src/routes/v1/proposals/schema/get-proposals/query.ts
+++ apps/api/src/routes/v1/proposals/schema/get-proposals/query.ts
@@ -1,9 +1,9 @@
 import { Type, Static } from 'typebox';
-import { status } from '../status';
+import { state } from '../state';
 
 export const query = Type.Object({
   dao_id: Type.Optional(Type.Integer({ minimum: 0 })),
-  status: Type.Optional(status),
+  status: Type.Optional(state),
   limit: Type.Integer({ default: 20, minimum: 0, maximum: 100 }),
   offset: Type.Integer({ default: 0, minimum: 0 }),
 });
