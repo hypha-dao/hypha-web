8f9751c9f31c7ef7ca97baf833426dc39570b13b
Dmitry E <dsanichweb@gmail.com>
2025-10-27 17:00:46 +0300

    feat(#1090): added db structure for keep events

---
+++ packages/storage-postgres/src/schema/event.ts
@@ -0,0 +1,38 @@
+import { InferSelectModel, sql } from 'drizzle-orm';
+import {
+  index,
+  integer,
+  jsonb,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const events = pgTable(
+  'events',
+  {
+    id: serial('id').primaryKey(),
+    type: text('type').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    // Polymorphic reference
+    referenceId: integer('reference_id').notNull(),
+    referenceEntity: text('reference_entity', {
+      enum: ['person', 'space', 'document', 'token'],
+    }).notNull(),
+    parameters: jsonb('params').notNull().default({}),
+  },
+  (table) => [
+    index('events_reference_idx').on(table.referenceEntity, table.referenceId),
+    index('events_type_idx').on(table.type),
+    index('events_created_at_idx').on(table.createdAt),
+  ],
+);
+
+export type Event = InferSelectModel<typeof events>;
+
+export type EventReference =
+  | { entity: 'person'; id: number }
+  | { entity: 'space'; id: number }
+  | { entity: 'document'; id: number }
+  | { entity: 'token'; id: number };
