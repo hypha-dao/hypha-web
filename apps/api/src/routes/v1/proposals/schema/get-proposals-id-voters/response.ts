c18a382bc4eef4b31af1a105a74fa798c45fd9b0
Sergey B. <sergey3bv@gmail.com>
2025-10-28 18:03:45 +0300

    feat(api): proposal voters endpoint schema

---
+++ apps/api/src/routes/v1/proposals/schema/get-proposals-id-voters/response.ts
@@ -0,0 +1,22 @@
+import { Type, Static } from 'typebox';
+import { userVote } from '../user-vote';
+
+const voter = Type.Object({
+  name: Type.String(),
+  surname: Type.String(),
+  avatarUrl: Type.Union([Type.String({ format: 'uri' }), Type.Null()]),
+  vote: userVote,
+  address: Type.String(),
+  timestamp: Type.Union([Type.String({ format: 'date-time' }), Type.Null()]),
+});
+
+export const response = Type.Object({
+  voters: Type.Array(voter),
+  meta: Type.Object({
+    total: Type.Integer(),
+    limit: Type.Integer(),
+    offset: Type.Integer(),
+  }),
+});
+
+export type Response = Static<typeof response>;
