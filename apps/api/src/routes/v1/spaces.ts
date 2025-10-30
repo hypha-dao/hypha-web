58327e87c5a4d901da2b52b6664e0b14a27e1ba3
Sergey B. <sergey3bv@gmail.com>
2025-10-30 13:09:33 +0300

    feat(api): removal of hard coded mocks

--- apps/api/src/routes/v1/spaces.ts
+++ apps/api/src/routes/v1/spaces.ts
@@ -1,21 +1,17 @@
 import { FastifyInstance } from 'fastify';
-import { z } from 'zod';
-import { spaceMock } from '../../mocks';
 
 export default async function spacesRoutes(app: FastifyInstance) {
   /**
    * GET /spaces/:id
    */
-  app.get('/:id', async (request, reply) => {
-    const { id } = z.object({ id: z.coerce.number() }).parse(request.params);
-
-    return reply.send({ ...spaceMock, id });
+  app.get('/:id', async (_, reply) => {
+    return reply.code(500).send({ message: 'Not implemented yet' });
   });
 
   /**
    * GET /spaces
    */
   app.get('/', async (_, reply) => {
-    return reply.send([spaceMock]);
+    return reply.code(500).send({ message: 'Not implemented yet' });
   });
 }
