8addf6c93ca60e4f96332bed56716c4ee5af9540
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-11-05 11:20:24 +0300

    feat(#1752): improved logic for displaying delegatedSpaces in my space page

--- packages/epics/src/spaces/hooks/use-member-web3-space-ids.ts
+++ packages/epics/src/spaces/hooks/use-member-web3-space-ids.ts
@@ -6,6 +6,8 @@ import {
   getMemberSpaces,
   publicClient,
   getSpacesForDelegate,
+  getDelegators,
+  getSpaceDetails,
 } from '@hypha-platform/core/client';
 
 export function useMemberWeb3SpaceIds({
@@ -26,9 +28,43 @@ export function useMemberWeb3SpaceIds({
           getSpacesForDelegate({ user: address as `0x${string}` }),
         ),
       ]);
+
+      const filteredDelegated = (
+        await Promise.all(
+          (delegatedSpaces ?? []).map(async (spaceId) => {
+            const delegators = await publicClient.readContract(
+              getDelegators({
+                user: address as `0x${string}`,
+                spaceId,
+              }),
+            );
+            const spaceDetailsRaw = await publicClient.readContract(
+              getSpaceDetails({ spaceId }),
+            );
+            const [
+              unity,
+              quorum,
+              votingPowerSource,
+              tokenAdresses,
+              members,
+              exitMethod,
+              joinMethod,
+              createdAt,
+              creator,
+              executor,
+            ] = spaceDetailsRaw;
+            const isValid = delegators.some((delegator: Address) =>
+              members.includes(delegator),
+            );
+            return isValid ? spaceId : null;
+          }),
+        )
+      ).filter((id): id is bigint => id !== null);
+
       const allSpaces = Array.from(
-        new Set([...(memberSpaces ?? []), ...(delegatedSpaces ?? [])]),
+        new Set([...(memberSpaces ?? []), ...filteredDelegated]),
       );
+
       return allSpaces;
     },
     { revalidateOnFocus: true },
