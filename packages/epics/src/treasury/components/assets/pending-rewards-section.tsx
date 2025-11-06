7494eb6f25b41cda04e715959658943bded50946
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-11-06 13:06:12 +0300

    feat(#1506): created PendingRewardsSection component

---
+++ packages/epics/src/treasury/components/assets/pending-rewards-section.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { FC } from 'react';
+import { SectionFilter } from '@hypha-platform/ui/server';
+import { type Person, usePendingRewards } from '@hypha-platform/core/client';
+import { AssetCard } from './asset-card';
+import { useUserAssetsSection } from '../../hooks';
+import { Button } from '@hypha-platform/ui';
+import { Loader2 } from 'lucide-react';
+
+const HYPHA_TOKEN_ADDRESS = '0x8b93862835C36e9689E9bb1Ab21De3982e266CD3';
+
+type PendingRewardsSectionProps = {
+  person: Person;
+};
+
+export const PendingRewardsSection: FC<PendingRewardsSectionProps> = ({
+  person,
+}) => {
+  const { filteredAssets, updateUserAssets } = useUserAssetsSection({
+    personSlug: person?.slug,
+  });
+
+  const { pendingRewards, isLoading, claim, isClaiming, updatePendingRewards } =
+    usePendingRewards({ user: person?.address as `0x${string}` });
+
+  const originalAsset = filteredAssets?.find(
+    (a) => a.address === HYPHA_TOKEN_ADDRESS,
+  );
+  const hyphaTokenAsset =
+    originalAsset && pendingRewards !== undefined
+      ? {
+          ...originalAsset,
+          value: Number(pendingRewards / 10n ** BigInt(18)),
+        }
+      : undefined;
+
+  const disableClaimButton = !(Number(pendingRewards) > 0) || isClaiming;
+
+  const onHandleClaim = async () => {
+    await claim();
+    await updatePendingRewards();
+    await updateUserAssets();
+  };
+
+  return (
+    <div className="flex flex-col w-full justify-center items-center gap-4">
+      <div className="w-full flex justify-between">
+        <SectionFilter label="Rewards" />
+        <Button
+          title={
+            disableClaimButton ? 'The reward value must be greater than 0' : ''
+          }
+          disabled={disableClaimButton}
+          onClick={onHandleClaim}
+        >
+          {isClaiming && <Loader2 className="animate-spin w-4 h-4" />}
+          Claim
+        </Button>
+      </div>
+      <div className="w-full">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-2 mt-2">
+          <AssetCard {...hyphaTokenAsset} />
+        </div>
+        {isLoading && <AssetCard isLoading />}
+      </div>
+    </div>
+  );
+};
