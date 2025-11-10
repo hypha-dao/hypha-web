8d390508b433e0c881d29e1111e2cee2e2376488
Sergey B. <sergey3bv@gmail.com>
2025-11-10 16:21:53 +0300

    feat(api): query to fetch tokens by addresses

--- apps/api/src/plugins/db-queries.ts
+++ apps/api/src/plugins/db-queries.ts
@@ -1,10 +1,11 @@
-import { eq, sql, and, inArray } from 'drizzle-orm';
+import { eq, sql, and, inArray, asc } from 'drizzle-orm';
 
 import {
   documents,
-  type Document as DbDocument,
+  tokens,
   spaces,
   people,
+  type Document as DbDocument,
   type Person as DbPerson,
   type Space as DbSpace,
 } from './db-schema';
@@ -196,4 +197,51 @@ export const peopleByAddresses = async (
       offset,
     },
   };
+};
+
+export const findTokensByAddresses = async (
+  { addresses }: { addresses: `0x${string}`[] },
+  { db }: DbConfig,
+) => {
+  const upperAddresses = addresses.map((addr) => addr.toUpperCase());
+
+  const results = await db
+    .select({
+      id: tokens.id,
+      spaceId: tokens.spaceId,
+      agreementId: tokens.agreementId,
+      name: tokens.name,
+      symbol: tokens.symbol,
+      maxSupply: tokens.maxSupply,
+      type: tokens.type,
+      iconUrl: tokens.iconUrl,
+      transferable: tokens.transferable,
+      isVotingToken: tokens.isVotingToken,
+      decayInterval: tokens.decayInterval,
+      decayPercentage: tokens.decayPercentage,
+      createdAt: tokens.createdAt,
+      address: tokens.address,
+      agreementWeb3Id: tokens.agreementWeb3Id,
+    })
+    .from(tokens)
+    .where(inArray(sql<string>`upper(${tokens.address})`, upperAddresses))
+    .groupBy(
+      tokens.id,
+      tokens.spaceId,
+      tokens.agreementId,
+      tokens.name,
+      tokens.symbol,
+      tokens.maxSupply,
+      tokens.type,
+      tokens.iconUrl,
+      tokens.transferable,
+      tokens.isVotingToken,
+      tokens.decayInterval,
+      tokens.decayPercentage,
+      tokens.createdAt,
+      tokens.address,
+      tokens.agreementWeb3Id,
+    );
+
+  return results;
 };
