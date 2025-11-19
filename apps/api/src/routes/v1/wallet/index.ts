4da5a8791bc71cbb1a06136c091c0fdbf1f208fd
Sergey B. <sergey3bv@gmail.com>
2025-11-19 17:37:16 +0300

    feat(api): usage of findPersonByAuth in wallet route

--- apps/api/src/routes/v1/wallet/index.ts
+++ apps/api/src/routes/v1/wallet/index.ts
@@ -39,11 +39,17 @@ export default async function walletRoutes(app: FastifyInstance) {
     {
       schema: { response: { 200: response } },
     },
-    async () => {
-      // TODO: find user's address
-      const userAddress = '0x';
+    async (request) => {
+      const authToken = request.headers.authorization?.split(' ').at(1);
+      // TODO: implement proper return
+      if (authToken == null) throw new Error('Unauthorized');
+
+      const userAddress = (await app.db.findPersonByAuth({ authToken }))
+        ?.address;
+      // TODO: implement proper return
+      if (userAddress == null) throw new Error('User not found');
       const rawBalances = await app.alchemy.getTokenBalanceByAddress(
-        userAddress,
+        userAddress as `0x${string}`,
       );
 
       const tokenAddresses = rawBalances
