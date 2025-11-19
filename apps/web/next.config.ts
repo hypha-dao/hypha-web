0c81746d674a2bd4e8f38e3711169cc2b377b5ca
Dmitry E <dsanichweb@gmail.com>
2025-11-19 11:01:12 +0300

    feat(#1124): added disable image optimiziing when needed Conflicts: 	apps/web/.env.template

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,7 +3,6 @@ import { i18nConfig } from '../../packages/i18n/src/i18n-config';
 
 const IMAGE_HOSTS = process.env.NEXT_PUBLIC_IMAGE_HOSTS?.split(', ') ?? [];
 const LOCALES = i18nConfig.locales;
-const isDevelop = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   headers: async () => {
@@ -29,7 +28,7 @@ const nextConfig: NextConfig = {
     ];
   },
   images: {
-    unoptimized: isDevelop,
+    unoptimized: process.env.DISABLE_IMAGE_OPTIMIZATION === 'true',
     remotePatterns: IMAGE_HOSTS.map((hostname) => ({
       protocol: 'https',
       hostname,
