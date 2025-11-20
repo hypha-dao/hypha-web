2fe071e3146179e029c2f7f2ac6d61d91f18bd21
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-11-20 14:51:00 +0300

    feat(#1726): updated getting recipient in ProposalMintItem

--- packages/epics/src/governance/components/proposal-mint-item.tsx
+++ packages/epics/src/governance/components/proposal-mint-item.tsx
@@ -4,6 +4,9 @@ import { usePersonByWeb3Address } from '../hooks';
 import { PersonAvatar } from '../../people/components/person-avatar';
 import { Skeleton } from '@hypha-platform/ui';
 import { formatCurrencyValue } from '@hypha-platform/ui-utils';
+import { useDbSpaces } from '../../hooks';
+import { EthAddress } from '../../people';
+import { Image } from '@hypha-platform/ui';
 
 interface ProposalMintItemProps {
   member: `0x${string}`;
@@ -11,17 +14,53 @@ interface ProposalMintItemProps {
 }
 
 export const ProposalMintItem = ({ member, number }: ProposalMintItemProps) => {
+  const { spaces: dbSpaces } = useDbSpaces({
+    parentOnly: false,
+  });
   const originalNumber = Number(number) / Number(10n ** 18n);
   const { person, isLoading } = usePersonByWeb3Address(member);
+
+  const space = dbSpaces.find(
+    (s) => s.address?.toLowerCase() === member.toLowerCase(),
+  );
+
   return (
     <div className="flex flex-col gap-3">
       <Skeleton loading={isLoading} className="h-7 w-full">
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
-            <PersonAvatar avatarSrc={person?.avatarUrl} size="md" />
-            <div className="text-1 w-full">
-              {person?.name} {person?.surname}
-            </div>
+            {person ? (
+              <>
+                <PersonAvatar avatarSrc={person?.avatarUrl} size="md" />
+                <div className="text-1 w-full">
+                  {person?.name} {person?.surname}
+                </div>
+              </>
+            ) : space ? (
+              <>
+                <Image
+                  className="rounded-lg w-[24px] h-[24px]"
+                  src={space?.logoUrl ?? '/placeholder/default-profile.svg'}
+                  width={24}
+                  height={24}
+                  alt={`${space?.title} logo`}
+                />
+                <div className="text-1 w-full">{space?.title}</div>
+              </>
+            ) : (
+              <>
+                <Image
+                  className="rounded-lg w-[24px] h-[24px]"
+                  src="/placeholder/default-profile.svg"
+                  width={24}
+                  height={24}
+                  alt="Default avatar"
+                />
+                <div className="text-1 w-full">
+                  <EthAddress address={member} />
+                </div>
+              </>
+            )}
           </div>
           <div className="text-1">
             {formatCurrencyValue(Number(originalNumber))}
