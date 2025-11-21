9bc94f5c252b722d0e7f024395ca32ae805199ca
Sergey B. <sergey3bv@gmail.com>
2025-11-21 17:24:08 +0300

    WIP(api): token endpoint

--- apps/api/src/routes/v1/wallet/token/index.ts
+++ apps/api/src/routes/v1/wallet/token/index.ts
@@ -2,10 +2,21 @@ import type { FastifyInstance } from 'fastify';
 import { schema, type Schema } from './schema';
 
 export default async function tokenRoutes(app: FastifyInstance) {
-  /*
+  /**
    * @summary Fetches transactions of a user by specified token
    */
-  app.get<Schema>('/:id', { schema }, async () => {
-    // TODO
+  app.get<Schema>('/:id', { schema }, async (req) => {
+    const authToken = req.headers.authorization?.split(' ').at(1);
+    // TODO: implement proper return
+    if (authToken == null) throw new Error('Unauthorized');
+
+    const userAddress = (await app.db.findPersonByAuth({ authToken }))?.address;
+    // TODO: implement proper return
+    if (userAddress == null) throw new Error('User not found');
+
+    const { id } = req.params;
+    const token = await app.db.findTokenById({ id });
+    // TODO: implement proper return
+    if (token == null) throw new Error('Token not found');
   });
 }
