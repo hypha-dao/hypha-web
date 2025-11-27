1f4d9c1e68b2c178f18c269911b8c5916bd6b48e
Sergey B. <sergey3bv@gmail.com>
2025-11-27 09:42:04 +0300

    WIP(api): recipients route

--- apps/api/src/routes/v1/token/_id/recipients/index.ts
+++ apps/api/src/routes/v1/token/_id/recipients/index.ts
@@ -2,5 +2,18 @@ import type { FastifyInstance } from 'fastify';
 import { schema, type Schema } from './schema';
 
 export default async function route(app: FastifyInstance) {
-  app.get<Schema>('/', { schema }, async () => {});
+  app.get<Schema>('/', { schema }, async (request) => {
+    const { id } = request.params;
+    const token = await app.db.findTokenById({ id });
+    if (token == null) {
+      // TODO: implement proper return
+      throw new Error('Token not found');
+    }
+    if (token.address == null) {
+      // TODO: implement proper return
+      throw new Error('Token does not have an address');
+    }
+
+    // TODO: get recipients
+  });
 }
