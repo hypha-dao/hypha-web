3a48a7864ca38d48845d1ad133a4f3d10078cc86
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-11-27 14:09:25 +0300

    feat(#1778): not show reward card if user not logged

--- apps/web/src/app/[lang]/profile/[personSlug]/_components/profile-tabs.tsx
+++ apps/web/src/app/[lang]/profile/[personSlug]/_components/profile-tabs.tsx
@@ -7,6 +7,7 @@ import {
   PendingRewardsSection,
 } from '@hypha-platform/epics';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@hypha-platform/ui';
+import { useAuthentication } from '@hypha-platform/authentication';
 import React from 'react';
 
 export const ProfileTabs = ({
@@ -18,6 +19,7 @@ export const ProfileTabs = ({
 }) => {
   const [activeTab, setActiveTab] = React.useState('treasury');
   const { person: authenticatedPerson } = useMe();
+  const { isAuthenticated } = useAuthentication();
   const isMyProfile =
     person?.address?.toLowerCase() ===
     authenticatedPerson?.address?.toLowerCase();
@@ -34,10 +36,12 @@ export const ProfileTabs = ({
         </TabsTrigger>
       </TabsList>
       <TabsContent value="treasury" className="flex flex-col gap-6">
-        <PendingRewardsSection
-          person={person as Person}
-          isMyProfile={isMyProfile}
-        />
+        {isAuthenticated && (
+          <PendingRewardsSection
+            person={person as Person}
+            isMyProfile={isMyProfile}
+          />
+        )}
         <UserAssetsSection
           isMyProfile={isMyProfile}
           personSlug={person?.slug || ''}
