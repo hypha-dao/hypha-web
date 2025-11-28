02a3e902c6830809eaa2e2bd6844995ef2692638
Sergey B. <sergey3bv@gmail.com>
2025-11-28 15:20:20 +0300

    feat(api): autoload for proposals routes

--- apps/api/src/routes/v1/index.ts
+++ apps/api/src/routes/v1/index.ts
@@ -1,12 +1,17 @@
 // apps/api/src/routes/v1/index.ts
 import { FastifyInstance } from 'fastify';
-import proposalsRoutes from './proposals';
+import { join } from 'node:path';
+import autoload from '@fastify/autoload';
 import dashboardRoutes from './dashboard';
 import spacesRoutes from './spaces';
 import walletRoutes from './wallet';
 
 export default async function v1Routes(app: FastifyInstance) {
-  app.register(proposalsRoutes, { prefix: '/proposals' });
+  app.register(autoload, {
+    dir: join(__dirname, 'proposals'),
+    routeParams: true,
+    options: { prefix: '/proposals' },
+  });
   app.register(dashboardRoutes, { prefix: '/dashboard' });
   app.register(spacesRoutes, { prefix: '/spaces' });
   app.register(walletRoutes, { prefix: '/wallet' });
