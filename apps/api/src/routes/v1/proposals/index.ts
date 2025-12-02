7fe636f469fcff7286faa136f6284bb705b5284e
Sergey B. <sergey3bv@gmail.com>
2025-12-02 17:35:38 +0300

    feat(api): route to create proposal

--- apps/api/src/routes/v1/proposals/index.ts
+++ apps/api/src/routes/v1/proposals/index.ts
@@ -156,7 +156,111 @@ export default async function proposalsRoutes(app: FastifyInstance) {
     };
   });
 
-  app.post<PostSchema>('/', { schema: postSchema }, async (_, reply) => {
-    return reply.code(500).send({ message: 'Not implemented yet' });
+  app.post<PostSchema>('/', { schema: postSchema }, async (request, reply) => {
+    const authToken = request.headers.authorization?.split(' ').at(1);
+    if (!authToken) {
+      // TODO: implement proper return
+      return reply.code(403).send();
+    }
+
+    const dbPerson = await app.db.findPersonByAuth({ authToken });
+    if (!dbPerson) {
+      // TODO: implement proper return
+      return reply.code(403).send();
+    }
+
+    const { creatorId } = request.body;
+    if (dbPerson.id !== creatorId) {
+      // TODO: implement proper return
+      return reply.code(403).send();
+    }
+
+    const web3Data = await (async () => {
+      const client = app.web3Client;
+
+      // TODO: use a proper default
+      const chainId = client.chain?.id || 8453;
+      type ChainId = keyof typeof daoProposalsImplementationAddress;
+      const contractAddress =
+        chainId in daoProposalsImplementationAddress &&
+        daoProposalsImplementationAddress[chainId as ChainId];
+      if (!contractAddress) {
+        console.error('Contract address was not found');
+
+        return;
+      }
+
+      const web3Id = request.body.web3ProposalId;
+      try {
+        return await client.readContract({
+          address: contractAddress,
+          abi: daoProposalsImplementationAbi,
+          functionName: 'getProposalCore',
+          args: [BigInt(web3Id)],
+        });
+      } catch (e) {
+        console.error('Error fetching proposal details:', e);
+      }
+    })();
+    if (!web3Data) {
+      // TODO: implement proper return
+      return reply.code(500).send();
+    }
+
+    const dbData = await app.db.createAgreement({
+      ...request.body,
+      authToken,
+      slug: request.body.slug ?? undefined,
+    });
+
+    const [
+      _spaceId,
+      _startTime,
+      endTime,
+      executed,
+      expired,
+      yesVotes,
+      noVotes,
+      totalVotingPowerAtSnapshot,
+      _creator,
+      _transactions,
+    ] = web3Data;
+
+    const quorumTotalVotingPowerNumber = Number(totalVotingPowerAtSnapshot);
+    const quorum =
+      quorumTotalVotingPowerNumber > 0
+        ? (Number(yesVotes + noVotes) / quorumTotalVotingPowerNumber) * 100
+        : 0;
+
+    const unityTotalVotingPowerNumber = Number(yesVotes) + Number(noVotes);
+    const unity =
+      unityTotalVotingPowerNumber > 0
+        ? (Number(yesVotes) / unityTotalVotingPowerNumber) * 100
+        : 0;
+
+    const status: State = executed || expired ? 'past' : 'active';
+
+    const responseBody = {
+      id: dbData.id,
+      title: dbData.title,
+      description: dbData.description || '',
+      label: 'agreement' as const,
+      image_URL: dbData.leadImage || '',
+      state: status,
+      unity,
+      quorum,
+      user_vote: null,
+      voting_deadline: new Date(Number(endTime) * 1000).toISOString(),
+      creatorId: dbData.creatorId,
+      creator: {
+        name: dbData.creator?.name || '',
+        surname: dbData.creator?.surname || '',
+        avatarUrl: dbData.creator?.avatarUrl || '',
+      },
+      createdAt: dbData.createdAt.toISOString(),
+      updatedAt: dbData.updatedAt.toISOString(),
+    };
+
+    return reply.code(201).send(responseBody);
   });
 }
