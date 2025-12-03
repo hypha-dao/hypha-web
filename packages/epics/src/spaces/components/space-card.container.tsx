a5655ef7c8ab1abb294a949bf0e2b1d99d4d987d
Dmitry E <dsanichweb@gmail.com>
2025-12-03 13:45:11 +0300

    feat(#1487): refactored space card container

--- packages/epics/src/spaces/components/space-card.container.tsx
+++ packages/epics/src/spaces/components/space-card.container.tsx
@@ -1,50 +1,60 @@
 'use client';
 
-import Link from 'next/link';
 import { Locale } from '@hypha-platform/i18n';
 import { Space } from '@hypha-platform/core/client';
 import { getDhoPathAgreements } from '../../common';
 import { SpaceCard } from './space-card';
+import { useRouter } from 'next/navigation';
 
 type SpaceCardContainerProps = {
   lang: Locale;
   spaces: Space[];
+  showExitButton?: boolean;
 };
 
 export const SpaceCardContainer = ({
   lang,
   spaces,
-}: SpaceCardContainerProps) => (
-  <div
-    data-testid="member-spaces-container"
-    className="grid grid-cols-1 sm:grid-cols-3 gap-2"
-  >
-    {spaces.map((space) =>
-      space.slug ? (
-        <div key={space.id}>
-          <Link
-            href={getDhoPathAgreements(lang, space.slug)}
-            aria-label={`View agreements for ${space.title}`}
-          >
-            <SpaceCard
-              description={space.description || ''}
-              icon={space.logoUrl || ''}
-              leadImage={space.leadImage || ''}
-              members={space.memberCount}
-              agreements={space.documentCount}
-              title={space.title || ''}
-              isSandbox={space.flags?.includes('sandbox') ?? false}
-              isDemo={space.flags?.includes('demo') ?? false}
-              web3SpaceId={space.web3SpaceId as number}
-              configPath={`${getDhoPathAgreements(
-                lang,
-                space.slug,
-              )}/space-configuration`}
-              createdAt={space.createdAt}
-            />
-          </Link>
-        </div>
-      ) : null,
-    )}
-  </div>
-);
+  showExitButton,
+}: SpaceCardContainerProps) => {
+  const router = useRouter();
+  return (
+    <div
+      data-testid="member-spaces-container"
+      className="grid grid-cols-1 sm:grid-cols-3 gap-2"
+    >
+      {spaces.map((space) =>
+        space.slug ? (
+          <div key={space.id}>
+            <span
+              className="cursor-pointer"
+              aria-label={`View agreements for ${space.title}`}
+              onClick={() => {
+                const path = getDhoPathAgreements(lang, space.slug);
+                router.push(path);
+              }}
+            >
+              <SpaceCard
+                description={space.description || ''}
+                icon={space.logoUrl || ''}
+                leadImage={space.leadImage || ''}
+                members={space.memberCount}
+                agreements={space.documentCount}
+                title={space.title || ''}
+                isSandbox={space.flags?.includes('sandbox') ?? false}
+                isDemo={space.flags?.includes('demo') ?? false}
+                web3SpaceId={space.web3SpaceId as number}
+                configPath={`${getDhoPathAgreements(
+                  lang,
+                  space.slug,
+                )}/space-configuration`}
+                createdAt={space.createdAt}
+                showExitButton={showExitButton}
+              />
+            </span>
+          </div>
+        ) : null,
+      )}
+    </div>
+  );
+};
