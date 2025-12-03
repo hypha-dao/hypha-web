5e45c2d2a3bf0c8812f04f1c610d9db3595d19ce
Dmitry E <dsanichweb@gmail.com>
2025-12-03 14:27:52 +0300

    feat(#1487): added exit space button at space card in my spaces

--- packages/epics/src/spaces/components/exit-space.tsx
+++ packages/epics/src/spaces/components/exit-space.tsx
@@ -5,9 +5,10 @@ import React from 'react';
 
 type ExitSpaceProps = {
   web3SpaceId: number;
+  exitButton?: React.ReactNode;
 };
 
-export const ExitSpace = ({ web3SpaceId }: ExitSpaceProps) => {
+export const ExitSpace = ({ web3SpaceId, exitButton }: ExitSpaceProps) => {
   const { isAuthenticated } = useAuthentication();
 
   const { exitSpace, isExitingSpace, isDisabled } = useExitSpace({
@@ -33,28 +34,32 @@ export const ExitSpace = ({ web3SpaceId }: ExitSpaceProps) => {
     }
   }, [exitSpace, revalidateIsMember]);
 
+  const button = exitButton ? (
+    exitButton
+  ) : (
+    <Button
+      colorVariant="neutral"
+      variant="outline"
+      title={
+        disabled
+          ? 'You need to be authorized and to be a member of current space to be able to exit'
+          : undefined
+      }
+      disabled={disabled}
+    >
+      Exit Space
+    </Button>
+  );
+
   return (
-    <>
-      <ConfirmDialog
-        title="Exit Space"
-        description="Do you really want to exit this space?"
-        customAcceptButtonText="Yes, leave"
-        customRejectButtonText="No, stay"
-        onAcceptClicked={handleExitSpace}
-      >
-        <Button
-          colorVariant="neutral"
-          variant="outline"
-          title={
-            disabled
-              ? 'You need to be authorized and to be a member of current space to be able to exit'
-              : undefined
-          }
-          disabled={disabled}
-        >
-          Exit Space
-        </Button>
-      </ConfirmDialog>
-    </>
+    <ConfirmDialog
+      title="Exit Space"
+      description="Do you really want to exit this space?"
+      customAcceptButtonText="Yes, leave"
+      customRejectButtonText="No, stay"
+      onAcceptClicked={handleExitSpace}
+    >
+      {button}
+    </ConfirmDialog>
   );
 };
