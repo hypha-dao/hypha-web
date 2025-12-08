2acb4e781ab2d6da52b90c5419c7db9dce993793
Dmitry E <dsanichweb@gmail.com>
2025-12-08 19:07:56 +0300

    hotfix: refactored

--- packages/ui-utils/src/canConvertToBigInt.ts
+++ packages/ui-utils/src/canConvertToBigInt.ts
@@ -1,12 +1,13 @@
-export function canConvertToBigInt(value: any): value is bigint {
-  if (typeof value === 'number') {
-    return Number.isInteger(value);
-  }
-  if (typeof value === 'string') {
-    return /^-?\d+$/.test(value);
-  }
-  if (typeof value === 'boolean') {
-    return true;
+type BigIntConvertible = bigint | number | string | boolean;
+
+export function canConvertToBigInt(value: unknown): value is BigIntConvertible {
+  switch (typeof value) {
+    case 'number':
+      return Number.isInteger(value);
+    case 'string':
+      return /^-?\d+$/.test(value.trim());
+    case 'boolean':
+      return true;
   }
   return false;
 }
