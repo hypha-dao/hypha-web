8253eee7e566a19861cdb50110e6c2a46972c68e
Sergey B. <sergey3bv@gmail.com>
2025-12-10 11:40:54 +0300

    feat(api): rate limit was added to spaces route

--- apps/api/src/routes/v1/spaces/index.ts
+++ apps/api/src/routes/v1/spaces/index.ts
@@ -1,11 +1,26 @@
 import type { FastifyInstance } from 'fastify';
 import { schema, type Schema } from './schema';
+import ratelimit from '@fastify/rate-limit';
 
 export default async function spacesRoutes(app: FastifyInstance) {
+  await app.register(ratelimit, {
+    global: false,
+    max: 100,
+    timeWindow: '1 minute',
+  });
+
   /**
    * @summary Get spaces by a user
    */
-  app.get<Schema>('/', { schema }, async (_, reply) => {
+  app.get<Schema>(
+    '/',
+    {
+      schema,
+      config: {
+        rateLimit: { max: 100, timeWindow: '1 minute' },
+      },
+    },
+  }, async (_, reply) => {
     return reply.notImplemented();
   });
 }
