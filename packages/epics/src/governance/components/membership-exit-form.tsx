44340351f95efabf483e3224666c9c91162bdc69
Dmitry E <dsanichweb@gmail.com>
2025-12-11 21:04:37 +0300

    feat(#661): added basic form for membership exit proposal creation

---
+++ packages/epics/src/governance/components/membership-exit-form.tsx
@@ -0,0 +1,158 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  createAgreementFiles,
+  schemaMembershipExit,
+  Space,
+  useJwt,
+  useMe,
+  useMembershipExitOrchestrator,
+} from '@hypha-platform/core/client';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { useScrollToErrors } from '../../hooks';
+import { useConfig } from 'wagmi';
+import { Button, Form, LoadingBackdrop, Separator } from '@hypha-platform/ui';
+import { CreateAgreementBaseFields } from '../../agreements';
+
+const combinedSchemaMembershipExit =
+  schemaMembershipExit.extend(createAgreementFiles);
+type FormValues = z.infer<typeof combinedSchemaMembershipExit>;
+
+interface MembershipExitFormProps {
+  successfulUrl: string;
+  backUrl?: string;
+  children?: React.ReactNode;
+  web3SpaceId?: number | null;
+  spaces?: Space[];
+}
+
+export const MembershipExitForm = ({
+  successfulUrl,
+  backUrl,
+  children,
+  web3SpaceId,
+  spaces,
+}: MembershipExitFormProps) => {
+  const { person } = useMe();
+
+  const formRef = React.useRef<HTMLFormElement>(null);
+  const form = useForm<FormValues>({
+    resolver: zodResolver(combinedSchemaMembershipExit),
+    mode: 'onChange',
+    defaultValues: {
+      label: 'Membership Exit',
+      title: '',
+      description: '',
+      leadImage: undefined,
+      attachments: undefined,
+      creatorId: person?.id,
+      spaceId: web3SpaceId ?? undefined,
+      member: undefined,
+    },
+  });
+
+  useScrollToErrors(form, formRef);
+
+  const { jwt } = useJwt();
+  const config = useConfig();
+  const {
+    membershipExitAction,
+    reset,
+    currentAction,
+    isError,
+    isPending,
+    progress,
+  } = useMembershipExitOrchestrator({ authToken: jwt, config, spaces });
+
+  /* const spaceAddress = form.watch('space');
+
+  const { hasTokenRequirements, hasEnoughTokens, missingTokenMessage } =
+    useSpaceTokenRequirementsByAddress({
+      spaceAddress,
+      spaces,
+  }); */
+
+  const handleCreate = async (data: FormValues) => {
+    if (!data.spaceId || !data.member) {
+      return;
+    }
+
+    /* if (hasTokenRequirements && !hasEnoughTokens) {
+      console.warn('Cannot submit proposal: not enough tokens.');
+      return;
+    } */
+
+    try {
+      await membershipExitAction({
+        ...data,
+        web3SpaceId: data.spaceId,
+        member: data.member,
+      });
+    } catch (error) {
+      console.error('Error creating membership exit proposal:', error);
+    }
+  };
+
+  return (
+    <LoadingBackdrop
+      showKeepWindowOpenMessage={true}
+      fullHeight={true}
+      progress={progress}
+      isLoading={isPending}
+      message={
+        isError ? (
+          <div>
+            <div className="flex flex-col">
+              <div>Ouh Snap. There was an error</div>
+              <Button onClick={reset}>Reset</Button>
+            </div>
+          </div>
+        ) : (
+          <div>{currentAction}</div>
+        )
+      }
+    >
+      <Form {...form}>
+        <form
+          ref={formRef}
+          onSubmit={form.handleSubmit(handleCreate)}
+          className="flex flex-col gap-5"
+        >
+          <CreateAgreementBaseFields
+            creator={{
+              avatar: person?.avatarUrl || '',
+              name: person?.name || '',
+              surname: person?.surname || '',
+            }}
+            successfulUrl={successfulUrl}
+            closeUrl={successfulUrl}
+            backUrl={backUrl}
+            backLabel="Back to Settings"
+            isLoading={false}
+            label="Membership Exit"
+            progress={progress}
+          />
+          {children}
+
+          <Separator />
+
+          <div className="flex justify-end w-full">
+            <Button
+              // disabled={hasTokenRequirements && !hasEnoughTokens}
+              type="submit"
+            >
+              Publish
+            </Button>
+          </div>
+
+          {/* {hasTokenRequirements && !hasEnoughTokens && (
+            <div className="text-error-11 text-2">{missingTokenMessage}</div>
+          )} */}
+        </form>
+      </Form>
+    </LoadingBackdrop>
+  );
+};
