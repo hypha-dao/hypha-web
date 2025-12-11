e3205a61e8bc16c165d1f110c23e755fda3809bf
Dmitry E <dsanichweb@gmail.com>
2025-12-11 13:44:30 +0300

    feat(#661): turned off exit method action

--- apps/web/src/app/[lang]/dho/[id]/_components/select-settings-action.tsx
+++ apps/web/src/app/[lang]/dho/[id]/_components/select-settings-action.tsx
@@ -92,7 +92,8 @@ export const SelectSettingsAction = ({
       baseTab: 'agreements',
       disabled: isPaymentExpired,
     },
-    {
+    //TODO: will be uncommented later
+    /*{
       group: 'Members',
       title: 'Exit Method (Coming Soon)',
       description:
@@ -100,7 +101,7 @@ export const SelectSettingsAction = ({
       href: '#',
       icon: <ExitIcon />,
       disabled: true,
-    },
+    },*/
     {
       group: 'Members',
       title: 'Membership Removal (Coming Soon)',
