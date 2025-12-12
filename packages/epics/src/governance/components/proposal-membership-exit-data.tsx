2b7f6847809267df0188198fa85b732a6c5f50c0
Dmitry E <dsanichweb@gmail.com>
2025-12-12 13:53:29 +0300

    feat(#661): added show proposal details for membership exit

---
+++ packages/epics/src/governance/components/proposal-membership-exit-data.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { Image } from '@hypha-platform/ui';
+import { usePersonByWeb3Address, useSpacesByWeb3IdsClient } from '../hooks';
+
+interface MembershipExitDataProps {
+  member?: string;
+  space?: bigint;
+}
+
+export const MembershipExitData = ({
+  member,
+  space,
+}: MembershipExitDataProps) => {
+  const { person } = usePersonByWeb3Address(member as `0x${string}`);
+  const { spaces } = useSpacesByWeb3IdsClient([space as bigint]);
+  const destructuredSpace = spaces?.[0];
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex w-full justify-between items-center">
+        <span className="text-2 text-neutral-11">Membership Exit</span>
+        <span className="flex gap-2 text-2 text-neutral-11">
+          <Image
+            className="w-[24px] h-[24px] rounded-lg"
+            src={
+              destructuredSpace?.logoUrl ??
+              '/placeholder/space-avatar-image.svg'
+            }
+            width={24}
+            height={24}
+            alt={`${destructuredSpace?.title} logo`}
+          />
+          <span>{destructuredSpace?.title}</span>
+        </span>
+      </div>
+      <div className="flex w-full justify-between items-center">
+        <span className="text-2 text-neutral-11">Delegated Voting Member</span>
+        <span className="flex gap-2 text-2 text-neutral-11">
+          <Image
+            className="rounded-lg"
+            src={person?.avatarUrl ?? '/placeholder/default-profile.svg'}
+            width={24}
+            height={24}
+            alt={`${person?.nickname} avatar`}
+          />
+          <span>
+            {person?.name} {person?.surname}
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+};
