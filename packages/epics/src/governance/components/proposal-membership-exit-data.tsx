47fde5e203ccfa222f11b05da472b8118b8a4346
Dmitry E <dsanichweb@gmail.com>
2025-12-22 13:31:07 +0300

    feat(#661): added support for space as an exiting member

--- packages/epics/src/governance/components/proposal-membership-exit-data.tsx
+++ packages/epics/src/governance/components/proposal-membership-exit-data.tsx
@@ -2,6 +2,8 @@
 
 import { Image } from '@hypha-platform/ui';
 import { usePersonByWeb3Address, useSpacesByWeb3IdsClient } from '../hooks';
+import { useDbSpaces } from '../../hooks';
+import { EthAddress } from '../../people';
 
 interface MembershipExitDataProps {
   member?: string;
@@ -15,22 +17,46 @@ export const MembershipExitData = ({
   const { person } = usePersonByWeb3Address(member as `0x${string}`);
   const { spaces } = useSpacesByWeb3IdsClient([space as bigint]);
   const destructuredSpace = spaces?.[0];
+  const { spaces: dbSpaces } = useDbSpaces({
+    parentOnly: false,
+  });
+  const exitingMemberSpace = dbSpaces.find(
+    (s) => s.address?.toLowerCase() === member?.toLowerCase(),
+  );
   return (
     <div className="flex flex-col gap-5">
       <div className="flex w-full justify-between items-center">
         <span className="text-2 text-neutral-11">Exiting Member</span>
-        <span className="flex gap-2 text-2 text-neutral-11">
-          <Image
-            className="rounded-lg"
-            src={person?.avatarUrl ?? '/placeholder/default-profile.svg'}
-            width={24}
-            height={24}
-            alt={`${person?.nickname} avatar`}
-          />
-          <span>
-            {person?.name} {person?.surname}
+        {person ? (
+          <span className="flex gap-2 text-2 text-neutral-11">
+            <Image
+              className="rounded-lg w-[24px] h-[24px]"
+              src={person?.avatarUrl ?? '/placeholder/default-profile.svg'}
+              width={24}
+              height={24}
+              alt={`${person?.nickname} avatar`}
+            />
+            <span className="text-nowrap">
+              {person?.name} {person?.surname}
+            </span>
           </span>
-        </span>
+        ) : exitingMemberSpace ? (
+          <span className="flex gap-2 text-2 text-neutral-11">
+            <Image
+              className="rounded-lg w-[24px] h-[24px]"
+              src={
+                exitingMemberSpace?.logoUrl ??
+                '/placeholder/default-profile.svg'
+              }
+              width={24}
+              height={24}
+              alt={`${exitingMemberSpace?.title} logo`}
+            />
+            <span className="text-nowrap">{exitingMemberSpace?.title}</span>
+          </span>
+        ) : (
+          <EthAddress address={member || ''} />
+        )}
       </div>
       <div className="flex w-full justify-between items-center">
         <span className="text-2 text-neutral-11">Exit From</span>
