8da7a11540a5db953ce778e25a0b59a4d6160083
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-12-29 13:32:01 +0300

    feat(#1908): updated signal and conversation cards

--- packages/epics/src/coherence/components/coherence-block.tsx
+++ packages/epics/src/coherence/components/coherence-block.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { useAuthentication } from '@hypha-platform/authentication';
 import { useCoherenceRecords } from '../hooks';
 import { ConversationSection } from './conversation-section';
 import { SignalSection } from './signal-section';
+import { Empty } from '../../common/empty';
 
 type CoherenceBlockProps = {};
 
 export function CoherenceBlock({}: CoherenceBlockProps) {
+  const { isAuthenticated } = useAuthentication();
   const {
     records: { signals, conversations },
     isLoading,
@@ -14,22 +17,30 @@ export function CoherenceBlock({}: CoherenceBlockProps) {
 
   return (
     <div className="flex flex-col gap-6 py-4">
-      <SignalSection
-        label="AI Signals"
-        hasSearch={false}
-        signals={signals}
-        isLoading={isLoading}
-        firstPageSize={9}
-        pageSize={15}
-      />
-      <ConversationSection
-        label="Conversations"
-        hasSearch={true}
-        conversations={conversations}
-        isLoading={isLoading}
-        firstPageSize={9}
-        pageSize={15}
-      />
+      {isAuthenticated ? (
+        <>
+          <SignalSection
+            label="AI Signals"
+            hasSearch={false}
+            signals={signals}
+            isLoading={isLoading}
+            firstPageSize={3}
+            pageSize={3}
+          />
+          <ConversationSection
+            label="Conversations"
+            hasSearch={true}
+            conversations={conversations}
+            isLoading={isLoading}
+            firstPageSize={4}
+            pageSize={2}
+          />
+        </>
+      ) : (
+        <Empty>
+          <p>Please, sign in to see signals and conversations</p>
+        </Empty>
+      )}
     </div>
   );
 }
--- packages/epics/src/coherence/components/signal-card.tsx
+++ packages/epics/src/coherence/components/signal-card.tsx
@@ -1,6 +1,8 @@
 import {
   BadgeItem,
+  BadgeProps,
   BadgesList,
+  Button,
   Card,
   CardContent,
   CardTitle,
@@ -8,6 +10,7 @@ import {
 } from '@hypha-platform/ui';
 import { Coherence } from '../types';
 import { stripDescription, stripMarkdown } from '@hypha-platform/ui-utils';
+import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons';
 
 type SignalCardProps = { isLoading: boolean };
 
@@ -16,20 +19,38 @@ export const SignalCard: React.FC<SignalCardProps & Coherence> = ({
   title,
   description,
   label,
+  labelType,
 }) => {
-  const badges: BadgeItem[] = [{ label }];
+  const badges: BadgeItem[] = [
+    {
+      label,
+      variant: 'solid',
+      colorVariant: (() => {
+        switch (labelType) {
+          case 'opportunity':
+            return 'accent';
+          case 'tensions':
+            return 'warn';
+          default:
+            return 'accent';
+        }
+      })(),
+    },
+  ];
   return (
     <Card className="h-full w-full space-y-5 pt-5">
       <CardContent className="relative space-y-4">
         <div className="flex flex-col items-start space-y-2">
-          <BadgesList isLoading={isLoading} badges={badges ?? []} />
+          {badges?.length > 0 && (
+            <BadgesList isLoading={isLoading} badges={badges ?? []} />
+          )}
           <Skeleton
             className="min-w-full"
             width="120px"
             height="18px"
             loading={isLoading}
           >
-            <CardTitle>{title}</CardTitle>
+            <CardTitle className="leading-5">{title}</CardTitle>
           </Skeleton>
         </div>
         <div className="flex flex-grow text-1 text-neutral-11">
@@ -48,6 +69,20 @@ export const SignalCard: React.FC<SignalCardProps & Coherence> = ({
               )}
             </div>
           </Skeleton>
+        </div>
+        <div className="flex flex-grow text-1 text-neutral-11 gap-3">
+          <Button variant="outline" colorVariant="accent">
+            <CheckIcon />
+            Worth Conversation
+          </Button>
+          <Button
+            variant="outline"
+            colorVariant="neutral"
+            className="bg-transparent text-neutral-11"
+          >
+            <Cross2Icon />
+            Not Relevant
+          </Button>
         </div>
       </CardContent>
     </Card>
