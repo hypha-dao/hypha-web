3b3155f564aa626c351319a623ed25c7a877f3a9
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2026-01-13 14:54:11 +0300

    feat(#1908): basic data structures for linking privy and matrix user

---
+++ packages/core/src/matrix/server/queries.ts
@@ -0,0 +1,20 @@
+import { MatrixUserLink } from '@hypha-platform/storage-postgres';
+import { DbConfig } from '../../server';
+
+export const findLinkByPrivyUserId = async (
+  { privyUserId }: { privyUserId: string },
+  { db }: DbConfig,
+): Promise<MatrixUserLink | null> => {
+  const response = await db.query.matrixUserLinks.findFirst({
+    where: (matrixUserLinks, { eq }) =>
+      eq(matrixUserLinks.privyUserId, privyUserId),
+  });
+
+  if (!response) {
+    return null;
+  }
+
+  return {
+    ...response,
+  };
+};
