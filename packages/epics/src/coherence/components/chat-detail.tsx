ec6a748c3f7e5afa2a8353635b804ecfa1c25116
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2026-01-22 19:42:18 +0300

    feat(#1908): updated chat detail ui

--- packages/epics/src/coherence/components/chat-detail.tsx
+++ packages/epics/src/coherence/components/chat-detail.tsx
@@ -39,17 +39,24 @@ export const ChatDetail = ({
 
   return (
     <div className="relative w-full h-full">
-      <div className="flex flex-col gap-5 pb-[70px]">
-        <div className="flex gap-2 justify-between">
-          <ChatHead
-            creator={creator}
-            isLoading={isLoading}
-            createDate={formatDate(conversation?.createdAt ?? new Date(), true)}
-          />
-          <div></div>
-          <ButtonClose closeUrl={closeUrl} />
+      <div className="fixed top-9 right-4 pt-5 pb-1 pl-4 md:pl-7 pr-4 md:pr-9 w-full md:w-[calc(var(--spacing-container-sm)_-_var(--spacing)_*_4)] bg-neutral-2 z-100">
+        <div className="flex flex-col gap-5 w-full h-full">
+          <div className="flex gap-2 justify-between">
+            <ChatHead
+              creator={creator}
+              isLoading={isLoading}
+              createDate={formatDate(
+                conversation?.createdAt ?? new Date(),
+                true,
+              )}
+            />
+            <div></div>
+            <ButtonClose closeUrl={closeUrl} />
+          </div>
+          <Separator />
         </div>
-        <Separator />
+      </div>
+      <div className="flex flex-col gap-5 pt-[70px] pb-[80px]">
         <div className="flex flex-col gap-3">
           <Skeleton width="80px" height="16px" loading={isLoading}>
             <Text className="text-3">{conversation?.title}</Text>
