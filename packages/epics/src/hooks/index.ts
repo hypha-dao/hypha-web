f60463c9346417f16ce5e834da84c523e022d63e
Dmitry E <dsanichweb@gmail.com>
2026-03-18 15:24:17 +0100

    feat(#1957): add useIsMobile hook

--- packages/epics/src/hooks/index.ts
+++ packages/epics/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './use-db-tokens';
+export * from './use-is-mobile';
 export * from './use-scroll-to-errors';
 export * from './use-db-spaces';
 export * from './use-resubmit-proposal-data';
