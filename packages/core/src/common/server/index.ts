cb552cce2837ee12fe4f86984f74d07dbf31ec80
Dmitry E <dsanichweb@gmail.com>
2026-03-19 16:12:15 +0300

    feat(#1827): unify supported currencies and add USD conversion service

--- packages/core/src/common/server/index.ts
+++ packages/core/src/common/server/index.ts
@@ -4,6 +4,7 @@ export * from './get-db';
 export * from './order';
 
 export * from './get-token-price';
+export * from './get-fiat-conversion-rate';
 export * from './get-transfers-by-address';
 export * from './get-token-balances-by-address';
 
