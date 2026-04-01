40f1e893005605cb0d7b4015eeb836f91a069e4a
Philipp Litzenberger <philipp@bendcircular.com>
2026-04-01 15:40:39 +0200

    feat(epics/coherence): add coherence-block top-level component

--- packages/ui/src/organisms/section-filter.tsx
+++ packages/ui/src/organisms/section-filter.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Text } from '@radix-ui/themes';
 import { Input } from '@hypha-platform/ui';
 import { SearchIcon } from 'lucide-react';
+import { cn } from '@hypha-platform/ui-utils';
 
 export type FilterOption = {
   label: string;
@@ -11,35 +12,70 @@ export type FilterOption = {
 type SectionFilterProps = {
   count?: number | string;
   label: string;
-  children?: React.ReactNode;
   hasSearch?: boolean;
   searchPlaceholder?: string;
+  inlineLabel?: boolean;
+  className?: string;
   onChangeSearch?: (term: string) => void;
+  children?: React.ReactNode;
 };
 
 export const SectionFilter: FC<SectionFilterProps> = ({
   count,
   label,
-  children,
   hasSearch,
   searchPlaceholder = 'Search',
+  inlineLabel = true,
+  className,
   onChangeSearch,
+  children,
 }) => {
   return (
     <div className="flex justify-between items-center w-full gap-4">
-      <Text className="text-4 capitalize text-nowrap">
-        {label} {count ? <>| {count}</> : null}
-      </Text>
-      {hasSearch ? (
-        <Input
-          className="w-full"
-          placeholder={searchPlaceholder}
-          leftIcon={<SearchIcon size="16px" />}
-          onChange={(e) => onChangeSearch?.(e.target.value)}
-        />
-      ) : null}
-      {children && (
-        <div className="flex items-center text-nowrap">{children}</div>
+      {inlineLabel ? (
+        <>
+          <Text className="text-4 capitalize text-nowrap">
+            {label} {count ? <>| {count}</> : null}
+          </Text>
+          {hasSearch ? (
+            <Input
+              className="w-full"
+              placeholder={searchPlaceholder}
+              leftIcon={<SearchIcon size="16px" />}
+              onChange={(e) => onChangeSearch?.(e.target.value)}
+            />
+          ) : null}
+          {children && (
+            <div className={cn('flex items-center text-nowrap', className)}>
+              {children}
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <div className="w-full flex flex-col gap-4">
+            <div className="flex flex-row w-full">
+              <Text className="text-4 capitalize text-nowrap">
+                {label} {count ? <>| {count}</> : null}
+              </Text>
+              {children && (
+                <div className={cn('flex items-center text-nowrap', className)}>
+                  {children}
+                </div>
+              )}
+            </div>
+            <div className="flex flex-row w-full">
+              {hasSearch ? (
+                <Input
+                  className="w-full"
+                  placeholder={searchPlaceholder}
+                  leftIcon={<SearchIcon size="16px" />}
+                  onChange={(e) => onChangeSearch?.(e.target.value)}
+                />
+              ) : null}
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
