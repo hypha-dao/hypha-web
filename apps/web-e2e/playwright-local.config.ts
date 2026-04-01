00d3e4de3c68e40fdb1c38c740bc9e74e5173d9f
Philipp Litzenberger <philipp@bendcircular.com>
2026-04-01 15:40:46 +0200

    chore: playwright config

--- apps/web-e2e/playwright-local.config.ts
+++ apps/web-e2e/playwright-local.config.ts
@@ -7,7 +7,5 @@ export default defineConfig({
     baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
   },
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-  ],
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
 });
