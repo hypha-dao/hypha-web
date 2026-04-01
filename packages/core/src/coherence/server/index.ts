f46e8f218d4fdb6dad7bad0816994344f5243d96
Philipp Litzenberger <philipp@bendcircular.com>
2026-04-01 15:40:37 +0200

    feat(core/coherence): add server actions and web3 adapters

---
+++ packages/core/src/coherence/server/index.ts
@@ -0,0 +1,4 @@
+export * from '../lib';
+export * from './actions';
+export * from './mutations';
+export * from './queries';
---
+++ packages/core/src/coherence/server/web3/index.ts
@@ -0,0 +1,2 @@
+export * from './get-all-coherences';
+export * from './get-coherence-by-slug';
